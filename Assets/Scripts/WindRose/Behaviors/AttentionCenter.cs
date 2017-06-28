cdc09ce08ff1f89758ec3114f69f228e3529c98b
Luis Masuelli <luisfmasuelli@gmail.com>
2017-06-27 23:27:48 -0500

    AttentionCenter captures a camera. This could perhaps be improved, but right now it only adjusts (x, y) keeping z intact. Perhaps fixing z to a value WRT to the object's z, and also ensuring the camera looks the object, would help.

---
+++ Assets/Scripts/WindRose/Behaviors/AttentionCenter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace WindRose
+{
+    namespace Behaviors
+    {
+        public class AttentionCenter : MonoBehaviour
+        {
+            public Camera followerCamera;
+
+            void Update()
+            {
+                if (followerCamera)
+                {
+                    followerCamera.transform.position = new Vector3(
+                        gameObject.transform.position.x,
+                        gameObject.transform.position.y,
+                        followerCamera.transform.position.z
+                    );
+                }
+            }
+        }
+    }
+}

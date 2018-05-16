1189edcaeb9ebf229b5edb28e830657f4aca1239
Luis Masuelli <luisfmasuelli@gmail.com>
2018-05-15 21:27:01 -0500

    Removing a comment

--- Assets/Scripts/WindRose/Types/States/MapState.cs
+++ Assets/Scripts/WindRose/Types/States/MapState.cs
@@ -363,7 +363,6 @@ namespace WindRose
 
                 public void SetBlocking(uint x, uint y, bool block)
                 {
-                    Debug.Log("Setting block x=" + x + ", y=" + y);
                     blockMask[x, y] = block;
                 }
 

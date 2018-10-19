dfe3fbc335498abb43c37f23d159dc1f09420958
Luis Masuelli <luisfmasuelli@gmail.com>
2018-10-18 23:10:33 -0500

    Adding algorithm to sort components by dependencies

--- Assets/Scripts/Support/Utils/Layout.cs
+++ Assets/Scripts/Support/Utils/Layout.cs
@@ -38,6 +38,13 @@ namespace Support
                 public UnserializableFieldException(string message, System.Exception inner) : base(message, inner) { }
             }
 
+            public class CircularDependencyUnsupportedException : Types.Exception
+            {
+                public CircularDependencyUnsupportedException() { }
+                public CircularDependencyUnsupportedException(string message) : base(message) { }
+                public CircularDependencyUnsupportedException(string message, System.Exception inner) : base(message, inner) { }
+            }
+
             public static T RequireComponentInParent<T>(MonoBehaviour script) where T : Component
             {
                 return RequireComponentInParent<T>(script.gameObject);
@@ -135,6 +142,93 @@ namespace Support
                         throw new UnserializableFieldException("The field " + pair.Key + " cannot be populated for type " + targetType.FullName);
                     }
                 }
+            }
+
+            /**
+             * Gets a set of all the types this component depends on.
+             */
+            public static HashSet<Type> GetDependencies(Component component)
+            {
+                return GetDependencies(component.GetType());
+            }
+
+            /**
+             * Gets a set of all the types the given component type depends on.
+             */
+            public static HashSet<Type> GetDependencies<C>() where C : Component
+            {
+                return GetDependencies(typeof(C));
+            }
+
+            /**
+             * Private implementation to get the component dependencies.
+             */
+            private static HashSet<Type> GetDependencies(Type componentType)
+            {
+                IEnumerable<RequireComponent> attributes = (from attribute in componentType.GetCustomAttributes(typeof(RequireComponent), false) select (attribute as RequireComponent));
+                HashSet<Type> types = new HashSet<Type>();
+                foreach (var attribute in attributes)
+                {
+                    types.Add(attribute.m_Type0);
+                    types.Add(attribute.m_Type1);
+                    types.Add(attribute.m_Type2);
+                }
+                types.Remove(null);
+                return types;
+            }
+
+            /**
+             * Flattens the dependencies of the components. The criteria of dependencies will
+             *   only account for types among the components, and not other potential types
+             *   they could depend on.
+             */
+            public static Component[] SortByDependencies(Component[] components)
+            {
+                HashSet<Type> consideredComponentTypes = new HashSet<Type>(from component in components select component.GetType());
+                List<Component> sourceComponentsList = new List<Component>(components);
+                List<Component> endComponentsList = new List<Component>();
+                HashSet<Type> fetchedTypes = new HashSet<Type>();
+
+                while(true)
+                {
+                    // We end the loop if there is nothing else in the
+                    //   source. The fetching terminated.
+                    if (sourceComponentsList.Count == 0) break;
+
+                    // On each iteration we will try adding at least one
+                    //   element to the final components list. Otherwise, we
+                    //   fail because the underlying reason is a circular
+                    //   dependency.
+                    bool foundTypeToAdd = false;
+
+                    // We iterate over all the components trying to find
+                    //   one to add. The criterion to accept one to add
+                    //   is that either it doesn't have dependencies or
+                    //   all its dependencies have already been processed.
+                    foreach(var component in sourceComponentsList)
+                    {
+                        HashSet<Type> dependencies = new HashSet<Type>(GetDependencies(component).Intersect(consideredComponentTypes));
+                        if (fetchedTypes.IsSupersetOf(dependencies))
+                        {
+                            // We mark that we found. Then we remove from source, add to end, and mark the type as fetched.
+                            foundTypeToAdd = true;
+                            sourceComponentsList.Remove(component);
+                            endComponentsList.Add(component);
+                            fetchedTypes.Add(component.GetType());
+                            break;
+                        }
+                    }
+
+                    // If no element was found to be added, we raise an
+                    //   exception because we found a circular dependency.
+                    if (!foundTypeToAdd)
+                    {
+                        throw new CircularDependencyUnsupportedException("Circular dependencies among components is unsupported when trying to sort them by dependencies");
+                    }
+                }
+
+                // This is the end result. Elements were added in-order.
+                return endComponentsList.ToArray();
             }
         }
     }

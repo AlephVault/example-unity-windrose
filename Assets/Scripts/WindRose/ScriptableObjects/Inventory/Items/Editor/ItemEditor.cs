d055fc8babee8afb1d9699311c9f315fd7da53fe
Luis Masuelli <luisfmasuelli@gmail.com>
2018-12-27 01:12:42 -0500

    Fixing a bug where index 0 was not tolerated among selections in editor

--- Assets/Scripts/WindRose/ScriptableObjects/Inventory/Items/Editor/ItemEditor.cs
+++ Assets/Scripts/WindRose/ScriptableObjects/Inventory/Items/Editor/ItemEditor.cs
@@ -32,7 +32,7 @@ namespace WindRose
                         mainRenderingStrategy = serializedObject.FindProperty("mainRenderingStrategy");
                     }
 
-                    private Object RelatedPopup(string caption, SerializedProperty arrayProperty, SerializedProperty selectedElement)
+                    private Object RelatedPopup(string caption, SerializedProperty arrayProperty, Object selectedElement)
                     {
                         List<Object> fetchedElements = new List<Object>();
                         List<GUIContent> fetchedElementTypeNames = new List<GUIContent>();
@@ -40,14 +40,16 @@ namespace WindRose
                         for(int i = 0; i < arrayProperty.arraySize; i++)
                         {
                             Object element = arrayProperty.GetArrayElementAtIndex(i).objectReferenceValue;
-                            fetchedElements.Add(element);
-                            fetchedElementTypeNames.Add(new GUIContent(element.GetType().FullName));
+                            if (element != null)
+                            {
+                                fetchedElements.Add(element);
+                                fetchedElementTypeNames.Add(new GUIContent(string.Format("{0} ({1})", element.name, element.GetType().Name)));
+                            }
                         }
 
-                        int index = fetchedElements.IndexOf(selectedElement.objectReferenceValue);
+                        int index = fetchedElements.IndexOf(selectedElement);
                         index = EditorGUILayout.Popup(new GUIContent(caption), index, fetchedElementTypeNames.ToArray());
-
-                        return index > 0 ? fetchedElements[index] : null;
+                        return index >= 0 ? fetchedElements[index] : null;
                     }
 
                     public override void OnInspectorGUI()
@@ -55,11 +57,12 @@ namespace WindRose
                         serializedObject.Update();
 
                         EditorGUILayout.PropertyField(quantifyingStrategy);
-                        EditorGUILayout.PropertyField(spatialStrategies);
-                        EditorGUILayout.PropertyField(usageStrategies);
-                        mainUsageStrategy.objectReferenceValue = RelatedPopup("Main Usage Strategy", usageStrategies, mainUsageStrategy);
-                        EditorGUILayout.PropertyField(renderingStrategies);
-                        mainRenderingStrategy.objectReferenceValue = RelatedPopup("Main Rendering Strategy", renderingStrategies, mainRenderingStrategy);
+                        EditorGUILayout.PropertyField(spatialStrategies, true);
+                        EditorGUILayout.PropertyField(usageStrategies, true);
+                        mainUsageStrategy.objectReferenceValue = RelatedPopup("Main Usage Strategy", usageStrategies, mainUsageStrategy.objectReferenceValue);
+                        EditorGUILayout.PropertyField(renderingStrategies, true);
+                        mainRenderingStrategy.objectReferenceValue = RelatedPopup("Main Rendering Strategy", renderingStrategies, mainRenderingStrategy.objectReferenceValue);
+
                         serializedObject.ApplyModifiedProperties();
                     }
                 }

be279df84a2f7f588cf697e6fa4a4fb7d7ebe8c4
Luis Masuelli <luisfmasuelli@gmail.com>
2019-04-13 10:52:58 -0500

    Tile utils to create bundled tiles from regular ones

---
+++ Assets/Scripts/WindRose/MenuActions/Tiles/BundledTileUtils.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+namespace WindRose
+{
+    namespace MenuActions
+    {
+        namespace Tiles
+        {
+            using Support.Utils;
+            using ScriptableObjects.Tiles;
+
+            /// <summary>
+            ///   Menu actions to create bundled tiles from regular ones.
+            /// </summary>
+            public static class BundledTileUtils
+            {
+                /// <summary>
+                ///   This method is used in the menu action: Assets > Create > Wind Rose > Tiles > Bundled Tiles (From 1+ selected non-bundled tiles).
+                ///   It creates one <see cref="BundledTile"/> for each selected <see cref="Tile"/> or <see cref="TileBase"/> not already being an
+                ///     instance of <see cref="BundledTile"/>, under the new /Bundled subdirectory, in the same directory, for each individual
+                ///     file and keeping the file name.
+                /// </summary>
+                [MenuItem("Assets/Create/Wind Rose/Tiles/Bundled Tiles (From 1+ selected non-bundled tiles)", false, priority = 202)]
+                public static void WrapIntoBundledTiles()
+                {
+                    foreach(TileBase tileBase in GetSelectedNonBundledTiles())
+                    {
+                        string path = AssetDatabase.GetAssetPath(tileBase.GetInstanceID());
+                        string parentPath = Path.GetDirectoryName(path);
+                        string fileName = Path.GetFileName(path);
+                        string bundledPath = Path.Combine(parentPath, "Bundled");
+                        if (!AssetDatabase.IsValidFolder(bundledPath))
+                        {
+                            AssetDatabase.CreateFolder(parentPath, "Bundled");
+                        }
+                        BundledTile bundledTile = ScriptableObject.CreateInstance<BundledTile>();
+                        Layout.SetObjectFieldValues(bundledTile, new Dictionary<string, object>() {
+                            { "sourceTile", tileBase }
+                        });
+                        AssetDatabase.CreateAsset(bundledTile, Path.Combine(bundledPath, fileName));
+                    }
+                }
+
+                /// <summary>
+                ///   Validates the menu item Assets > Create > Wind Rose > Tiles > Bundled Tiles (From 1+ selected non-bundled tiles).
+                ///   It enables such menu option when 1 or more <see cref="Tile"/> or <see cref="TileBase"/> objects are selected in
+                ///     the scene editor.
+                /// </summary>
+                [MenuItem("Assets/Create/Wind Rose/Tiles/Bundled Tiles (From 1+ selected non-bundled tiles)", true)]
+                public static bool CanWrapIntoBundledTiles()
+                {
+                    return GetSelectedNonBundledTiles().Count() > 0;
+                }
+
+                private static IEnumerable<TileBase> GetSelectedNonBundledTiles()
+                {
+                    foreach(TileBase tileBase in TileUtils.GetSelectedAssets<TileBase>())
+                    {
+                        if (!(tileBase is BundledTile)) yield return tileBase;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WindRose/MenuActions/Tiles/TileUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+
+namespace WindRose
+{
+    namespace MenuActions
+    {
+        namespace Tiles
+        {
+            static class TileUtils
+            {
+                public static IEnumerable<T> GetSelectedAssets<T>() where T : Object
+                {
+                    foreach(string assetGUID in Selection.assetGUIDs)
+                    {
+                        string path = AssetDatabase.GUIDToAssetPath(assetGUID);
+                        T asset = AssetDatabase.LoadAssetAtPath<T>(path);
+                        if (asset != null)
+                            yield return asset; 
+                    }
+                }
+            }
+        }
+    }
+}

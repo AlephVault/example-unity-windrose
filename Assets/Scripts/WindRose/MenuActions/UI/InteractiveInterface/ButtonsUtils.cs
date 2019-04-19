d541b5db2b1e1d492f10990390c22f3da4b8d486
Luis Masuelli <luisfmasuelli@gmail.com>
2019-04-19 18:15:23 -0500

    Buttons interactor dialog. Incomplete. Still to do: the logic! Also, an unused class removed

---
+++ Assets/Scripts/WindRose/MenuActions/UI/InteractiveInterface/ButtonsUtils.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+namespace WindRose
+{
+    namespace MenuActions
+    {
+        namespace UI
+        {
+            namespace InteractiveInterface
+            {
+                using GabTab.Behaviours;
+
+                /// <summary>
+                ///   Menu actions to create button sets (1, 2 or 3 buttons) to be used
+                ///     inside an <see cref="InteractiveInterface"/>.
+                /// </summary>
+                public static class ButtonsUtils
+                {
+                    private class ButtonsSettings
+                    {
+                        public string key = "";
+                        public string caption = "Button";
+                        public Color32 normalColor = new Color32(255, 255, 255, 255);
+                        public Color32 highlightColor = new Color32(245, 245, 245, 255);
+                        public Color32 pressedColor = new Color32(200, 200, 200, 255);
+                        public Color32 disabledColor = new Color32(200, 200, 200, 255);
+
+                        public ButtonsSettings(string key, string caption)
+                        {
+                            this.key = key;
+                            this.caption = caption;
+                        }
+                    }
+
+                    private class CreateButtonsInteractorWindow : EditorWindow
+                    {
+                        private string buttonsInteractorName = "New Buttons Interactor";
+                        private ButtonsSettings[] buttons = new ButtonsSettings[] {
+                            new ButtonsSettings("button-1", "Button 1"), new ButtonsSettings("button-2", "Button 2"), new ButtonsSettings("button-3", "Button 3")
+                        };
+                        private int buttonsCount = 1;
+                        private bool withBackground = false;
+                        private Color backgroundTint = Color.white;
+                        public Transform selectedTransform = null;
+
+                        private void ButtonsSettingsGUI(int index, ButtonsSettings settings, GUIStyle style)
+                        {
+                            settings.key = MenuActionUtils.EnsureNonEmpty(EditorGUILayout.TextField("Button key", settings.key), "button-" + index);
+                            EditorGUILayout.BeginVertical(style);
+                            settings.caption = MenuActionUtils.EnsureNonEmpty(EditorGUILayout.TextField("Caption", settings.caption), "Button " + index);
+                            if (settings.caption == "") settings.caption = "Button " + index;
+                            settings.normalColor = EditorGUILayout.ColorField("Normal color", settings.normalColor);
+                            settings.highlightColor = EditorGUILayout.ColorField("Highlight color", settings.highlightColor);
+                            settings.pressedColor = EditorGUILayout.ColorField("Pressed color", settings.pressedColor);
+                            settings.disabledColor = EditorGUILayout.ColorField("Disabled color", settings.disabledColor);
+                            EditorGUILayout.EndVertical();
+                        }
+
+                        private void AllButtonsSettingsGUI(GUIStyle style)
+                        {
+                            buttonsCount = EditorGUILayout.IntSlider("Buttons #", buttonsCount, 1, 3);
+                            for(int i = 0; i < buttonsCount; i++)
+                            {
+                                ButtonsSettingsGUI(i, buttons[i], style);
+                            }
+                        }
+
+                        private void OnGUI()
+                        {
+                            GUIStyle longLabelStyle = MenuActionUtils.GetSingleLabelStyle();
+                            GUIStyle indentedStyle = MenuActionUtils.GetIndentedStyle();
+
+                            titleContent = new GUIContent("Wind Rose - Creating a new Buttons Interactor");
+
+                            Rect contentRect = EditorGUILayout.BeginVertical();
+                            EditorGUILayout.LabelField("This wizard will create an interactor with one, two, or three buttons.", longLabelStyle);
+
+                            buttonsInteractorName = MenuActionUtils.EnsureNonEmpty(EditorGUILayout.TextField("Object name", buttonsInteractorName), "New Buttons Interactor");
+
+                            AllButtonsSettingsGUI(indentedStyle);
+                            withBackground = EditorGUILayout.ToggleLeft("Add background", withBackground);
+                            if (withBackground)
+                            {
+                                EditorGUILayout.BeginVertical(indentedStyle);
+                                backgroundTint = EditorGUILayout.ColorField("Background tint", backgroundTint);
+                                EditorGUILayout.EndVertical();
+                            }
+                            if (GUILayout.Button("Create Buttons Interactor"))
+                            {
+                                Execute();
+                            }
+                            EditorGUILayout.EndVertical();
+
+                            if (contentRect.size != Vector2.zero)
+                            {
+                                minSize = contentRect.max + contentRect.min;
+                                maxSize = minSize;
+                            }
+                        }
+
+                        private void Execute()
+                        {
+                            InteractorUtils.AddBackground(selectedTransform.GetComponent<InteractiveInterface>(), buttonsInteractorName, withBackground, backgroundTint);
+                            // Close();
+                        }
+                    }
+
+                    /// <summary>
+                    ///   This method is used in the menu action: GameObject > Wind Rose > UI > HUD > Interactive Interface > Create Buttons Interactor.
+                    ///   It creates a <see cref="InteractiveInterface"/>, with their inner message component, in the scene.
+                    /// </summary>
+                    [MenuItem("GameObject/Wind Rose/UI/HUD/Interactive Interface/Create Buttons Interactor", false, 11)]
+                    public static void CreateInteractiveInterface()
+                    {
+                        CreateButtonsInteractorWindow window = ScriptableObject.CreateInstance<CreateButtonsInteractorWindow>();
+                        window.maxSize = new Vector2(400, 176);
+                        window.minSize = window.maxSize;
+                        window.selectedTransform = Selection.activeTransform;
+                        window.ShowUtility();
+                    }
+
+                    /// <summary>
+                    ///   Validates the menu item: GameObject > Wind Rose > UI > HUD > Interactive Interface > Create Buttons Interactor.
+                    ///   It enables such menu option when an <see cref="InteractiveInterface"/> is selected in the scene hierarchy.
+                    /// </summary>
+                    [MenuItem("GameObject/Wind Rose/UI/HUD/Interactive Interface/Create Buttons Interactor", true)]
+                    public static bool CanCreateInteractiveInterface()
+                    {
+                        return Selection.activeTransform != null && Selection.activeTransform.GetComponent<InteractiveInterface>();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WindRose/MenuActions/UI/InteractiveInterface/InteractorUtils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+namespace WindRose
+{
+    namespace MenuActions
+    {
+        namespace UI
+        {
+            namespace InteractiveInterface
+            {
+                using Support.Utils;
+                using GabTab.Behaviours;
+
+                /// <summary>
+                ///   Utility class holding features common to almost all interactors.
+                /// </summary>
+                static class InteractorUtils
+                {
+                    /// <summary>
+                    ///   Creates a background that uses to be common to most interactors.
+                    ///   The background may be visible or invisible, but it is always
+                    ///     present in at least specific kind of interactors (like text
+                    ///     or buttons).
+                    /// </summary>
+                    /// <param name="parent">The interface the new object will be attached to</param>
+                    /// <param name="objectName">The name of the new object</param>
+                    /// <param name="addBackground">Whether the background will be set, or empty</param>
+                    /// <param name="backgroundTint">The tint to apply in the background</param>
+                    public static GameObject AddBackground(InteractiveInterface parent, string objectName, bool addBackground, Color backgroundTint)
+                    {
+                        GameObject interactorObject = new GameObject(objectName);
+                        interactorObject.transform.parent = parent.transform;
+                        Layout.AddComponent<Hideable>(interactorObject);
+                        Image interactorImage = Layout.AddComponent<Image>(interactorObject);
+
+                        if (addBackground)
+                        {
+                            interactorImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+                            interactorImage.type = Image.Type.Sliced;
+                            interactorImage.color = backgroundTint;
+                            interactorImage.enabled = true;
+                        }
+                        else
+                        {
+                            interactorImage.enabled = false;
+                        }
+
+                        RectTransform interactorRectTransformComponent = interactorObject.GetComponent<RectTransform>();
+                        Rect canvasRect = parent.transform.parent.GetComponent<RectTransform>().rect;
+                        float interactorOffset = Values.Max(canvasRect.height, canvasRect.width) * 0.01f;
+                        interactorRectTransformComponent.pivot = new Vector2(0.5f, 0);
+                        interactorRectTransformComponent.anchorMin = Vector2.zero;
+                        interactorRectTransformComponent.anchorMax = new Vector2(1f, 0.3f);
+                        interactorRectTransformComponent.offsetMin = Vector2.one * interactorOffset;
+                        interactorRectTransformComponent.offsetMax = new Vector2(-interactorOffset, 0);
+
+                        return interactorObject;
+                    }
+                }
+            }
+        }
+    }
+}

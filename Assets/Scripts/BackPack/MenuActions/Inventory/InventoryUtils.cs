ca09d03decc446c1521554d601768e7dfc44caf8
Luis Masuelli <luisfmasuelli@gmail.com>
2020-01-24 23:32:45 -0500

    UI for inventory creator utility, done

--- Assets/Scripts/BackPack/MenuActions/Inventory/InventoryUtils.cs
+++ Assets/Scripts/BackPack/MenuActions/Inventory/InventoryUtils.cs
@@ -19,13 +19,12 @@ namespace BackPack
             /// </summary>
             public class InventoryUtils
             {
-				// TODO change these ones later.
-				private const Vector2 windowSize = new Vector2(360, 110);
-				private const Vector2 windowPos = new Vector2(300, 200);
-
-                public class CreateSingleInventoryViewWindow : EditorWindow
+                public class CreateBasicInventoryViewWindow : EditorWindow
                 {
 					public Transform selectedTransform = null;
+
+                    // TODO change these ones later.
+                    private Vector2 windowSize = new Vector2(963, 689);
 
                     // Main container properties
                     private Color backgroundColor = Color.white;
@@ -33,7 +32,7 @@ namespace BackPack
                     private int rows = 2;
                     private int columns = 5;
 
-                    // Properties of the selection label
+                    // Properties of labels
                     private int selectedItemLabelHeight = 20;
                     private int selectedItemLabelFontSize = 19;
 
@@ -44,7 +43,7 @@ namespace BackPack
                     private ColorBlock nextPageButtonColor = MenuActionUtils.DefaultColors();
 
                     // Properties of the cell
-                    private Color backgroundCellColor = new Color32(200, 200, 200, 255);
+                    private Color cellColor = new Color32(200, 200, 200, 255);
                     private bool useCustomGapsForCells = false;
                     private int verticalCellGapSize = 4;
                     private int horizontalCellGapSize = 4;
@@ -76,40 +75,160 @@ namespace BackPack
 							"There is a default implementation (called the 'basic single & simple' one) that will be used, and " +
 							"is compatible with single-container inventories, and simple (icon, text, quantity) items, so the " +
 							"inventory to connect must be compatible (this package provides the Single Simple Inventory which " +
-							"satisfies both requirements and so it can be connected to the view being created by this wizard)"
+							"satisfies both requirements and so it can be connected to the view being created by this wizard).",
+                            longLabelStyle
 						);
 
-						///////////////////////////////////////////////////////
-						/// First, fix some values and derivate the other ones.
-						///////////////////////////////////////////////////////
+                        Debug.LogFormat("Size: {0}, Position: {1}", position.size, position.position);
 
-						bool customGapsForCellDisabled = !useCustomGapsForCells;
+                        EditorGUILayout.Space();
 
-						if (customGapsForCellDisabled) {
-							horizontalCellGapSize = gapSize;
-							verticalCellGapSize = gapSize;
-						}
+                        /// General Section Styles
+                        EditorGUILayout.LabelField("General Styles", longLabelStyle);
+                        EditorGUILayout.BeginHorizontal();
+                        backgroundColor = EditorGUILayout.ColorField("Background Color", backgroundColor);
+                        gapSize = EditorGUILayout.IntField("Gap Size", gapSize);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUILayout.BeginHorizontal();
+                        rows = EditorGUILayout.IntField("Rows", rows);
+                        columns = EditorGUILayout.IntField("Columns", columns);
+                        EditorGUILayout.EndHorizontal();
+                        // Fix some values (according to restrictions).
+                        gapSize = Values.Max(0, gapSize);
+                        rows = Values.Max(1, rows);
+                        columns = Values.Max(1, columns);
 
-						// Now the derivated values:
-						int cellWidth = 2 * horizontalCellPadding + iconWidth;
-						int cellHeight = 2 * verticalCellPadding + iconHeight;
-						int gridWidth = columns * cellWidth + (columns - 1) * horizontalCellGapSize + 1;
-						int gridHeight = rows * cellHeight + (rows - 1) * verticalCellGapSize + 1;
-						int controlWidth = gridWidth + 2 * gapSize;
-						int controlHeight = gridHeight + pageLabelHeight + selectedItemLabelHeight + 4 * gapSize;
-						int buttonHeight = pageLabelHeight;
-						int buttonWidth = pageLabelHeight;
-						int pageLabelWidth = controlWidth - 4 * gapSize - 2 * buttonWidth;
-						int selectedItemLabelWidth = gridWidth;
+                        EditorGUILayout.Space();
 
-						//////////////////////////////////
-						/// Now, the appropriate controls.
-						//////////////////////////////////
+                        /// Header Styles
+                        EditorGUILayout.LabelField("Header Controls' Styles", longLabelStyle);
+                        EditorGUILayout.BeginHorizontal();
+                        pageLabelHeight = EditorGUILayout.IntField("Page Label Height", pageLabelHeight);
+                        pageLabelFontSize = EditorGUILayout.IntField("Page Label Fond Size", pageLabelFontSize);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUILayout.BeginHorizontal();
+                        EditorGUILayout.BeginVertical();
+                        EditorGUILayout.LabelField("\"Previos\" Button");
+                        prevPageButtonColor = MenuActionUtils.ColorsGUI(prevPageButtonColor);
+                        EditorGUILayout.EndVertical();
+                        EditorGUILayout.BeginVertical();
+                        EditorGUILayout.LabelField("\"Next\" Button");
+                        nextPageButtonColor = MenuActionUtils.ColorsGUI(nextPageButtonColor);
+                        EditorGUILayout.EndVertical();
+                        EditorGUILayout.EndHorizontal();
+                        // Fix some values (according to restrictions).
+                        pageLabelFontSize = Values.Max(1, pageLabelFontSize);
+                        pageLabelHeight = Values.Max(1, pageLabelHeight);
+
+                        EditorGUILayout.Space();
+
+                        /// Cell Styles
+                        EditorGUILayout.LabelField("Cell Styles", longLabelStyle);
+                        EditorGUILayout.BeginHorizontal();
+                        cellColor = EditorGUILayout.ColorField("Background Color", cellColor);
+                        useCustomGapsForCells = EditorGUILayout.ToggleLeft("Use Custom Gaps", useCustomGapsForCells);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUI.BeginDisabledGroup(!useCustomGapsForCells);
+                        EditorGUILayout.BeginHorizontal();
+                        horizontalCellGapSize = EditorGUILayout.IntField("Horizontal Gap", useCustomGapsForCells ? horizontalCellGapSize : gapSize);
+                        verticalCellGapSize = EditorGUILayout.IntField("Vertical Gap", useCustomGapsForCells ? verticalCellGapSize : gapSize);
+                        horizontalCellGapSize = Values.Max(0, horizontalCellGapSize);
+                        verticalCellGapSize = Values.Max(0, verticalCellGapSize);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUI.EndDisabledGroup();
+                        EditorGUILayout.BeginHorizontal();
+                        horizontalCellPadding = EditorGUILayout.IntField("Horizontal Padding", horizontalCellPadding);
+                        verticalCellPadding = EditorGUILayout.IntField("Vertical Padding", verticalCellPadding);
+                        horizontalCellPadding = Values.Max(0, horizontalCellPadding);
+                        verticalCellPadding = Values.Max(0, verticalCellPadding);                       
+                        EditorGUILayout.EndHorizontal();
+
+                        EditorGUILayout.Space();
+
+                        EditorGUILayout.LabelField("Cell Content Styles", longLabelStyle);
+                        EditorGUILayout.BeginHorizontal();
+                        iconWidth = EditorGUILayout.IntField("Icon Width", iconWidth);
+                        iconHeight = EditorGUILayout.IntField("Icon Height", iconHeight);
+                        iconWidth = Values.Max(0, iconWidth);
+                        iconHeight = Values.Max(0, iconHeight);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUILayout.BeginHorizontal();
+                        labelWidth = EditorGUILayout.IntField("Label Width", labelWidth);
+                        labelHeight = EditorGUILayout.IntField("Label Height", labelHeight);
+                        labelWidth = Values.Max(0, labelWidth);
+                        labelHeight = Values.Max(0, labelHeight);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUILayout.BeginHorizontal();
+                        labelFontSize = EditorGUILayout.IntField("Label Font Size", labelFontSize);
+                        labelBottomMargin = EditorGUILayout.IntField("Bottom Margin", labelBottomMargin);
+                        labelFontSize = Values.Max(0, labelFontSize);
+                        labelBottomMargin = Values.Max(0, labelBottomMargin);
+                        EditorGUILayout.EndHorizontal();
+
+                        EditorGUILayout.Space();
+
+                        EditorGUILayout.LabelField("Final Cell Styles", longLabelStyle);
+                        EditorGUI.BeginDisabledGroup(true);
+                        EditorGUILayout.BeginHorizontal();
+                        int cellWidth = EditorGUILayout.IntField("Final Width", 2 * horizontalCellPadding + iconWidth);
+                        int cellHeight = EditorGUILayout.IntField("Final Height", 2 * verticalCellPadding + iconHeight);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUI.EndDisabledGroup();
+
+                        EditorGUILayout.Space();
+
+                        EditorGUILayout.LabelField("Grid Styles");
+                        EditorGUI.BeginDisabledGroup(true);
+                        EditorGUILayout.BeginHorizontal();
+                        int gridWidth = EditorGUILayout.IntField("Final Grid Width", columns * cellWidth + (columns - 1) * horizontalCellGapSize + 1);
+                        int gridHeight = EditorGUILayout.IntField("Final Grid Height", rows * cellHeight + (rows - 1) * verticalCellGapSize + 1);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUI.EndDisabledGroup();
+
+                        EditorGUILayout.Space();
+
+                        EditorGUILayout.LabelField("Final Header Controls' Styles");
+                        EditorGUI.BeginDisabledGroup(true);
+                        EditorGUILayout.BeginHorizontal();
+                        int buttonWidth = EditorGUILayout.IntField("Final Button Width", pageLabelHeight);
+                        int buttonHeight = EditorGUILayout.IntField("Final Button Height", pageLabelHeight);
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUILayout.BeginHorizontal();
+                        int pageLabelWidth = gridWidth - 2 * gapSize - 2 * buttonWidth;
+                        pageLabelWidth = Values.Max(0, pageLabelWidth);
+                        EditorGUILayout.Space();
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUI.EndDisabledGroup();
+
+                        EditorGUILayout.Space();
+
+                        EditorGUILayout.LabelField("Selected Item Label Styles");
+                        EditorGUILayout.BeginHorizontal();
+                        selectedItemLabelHeight = EditorGUILayout.IntField("Label Height", selectedItemLabelHeight);
+                        EditorGUI.BeginDisabledGroup(true);
+                        int selectedItemLabelWidth = gridWidth;
+                        EditorGUI.EndDisabledGroup();
+                        EditorGUILayout.EndHorizontal();
+                        selectedItemLabelFontSize = EditorGUILayout.IntField("Font Size", selectedItemLabelFontSize);
+
+                        EditorGUILayout.Space();
+
+                        EditorGUILayout.LabelField("Final Control Styles");
+                        EditorGUILayout.BeginHorizontal();
+                        EditorGUI.BeginDisabledGroup(true);
+                        int controlWidth = EditorGUILayout.IntField("Overall Width", gridWidth + 2 * gapSize);
+                        int controlHeight = EditorGUILayout.IntField("Overall Height", gridHeight + pageLabelHeight + selectedItemLabelHeight + 4 * gapSize);
+                        EditorGUI.EndDisabledGroup();
+                        EditorGUILayout.EndHorizontal();
+
+                        EditorGUILayout.Space();
+
+                        if (GUILayout.Button("Create Inventory")) Execute();
                     }
 
                     private void Execute()
                     {
-
+                        Close();
                     }
                 }
 
@@ -117,18 +236,18 @@ namespace BackPack
 				///   This method is used in the assets menu action: GameObject > Back Pack > Inventory > Create Basic Inventory.
 				/// </summary>
 				[MenuItem("GameObject/Back Pack/Inventory/Create Basic Inventory", false, 11)]
-				public static void AddBag()
+				public static void AddBasicInventory()
 				{
-					CreateSingleInventoryViewWindow window = ScriptableObject.CreateInstance<CreateSingleInventoryViewWindow>();
+					CreateBasicInventoryViewWindow window = ScriptableObject.CreateInstance<CreateBasicInventoryViewWindow>();
 					window.selectedTransform = Selection.activeTransform;
-					window.position = new Rect(windowPos, windowSize);
+					window.position = new Rect(new Vector2(139, 247), Vector2.zero);
 					window.ShowUtility();
 				}
 
 				[MenuItem("GameObject/Back Pack/Inventory/Create Basic Inventory", true)]
-				public static bool CanAddBag()
+				public static bool CanAddBasicInventory()
 				{
-					Selection.activeTransform != null && Selection.activeTransform.GetComponent<Canvas>();
+					return Selection.activeTransform != null && Selection.activeTransform.GetComponent<Canvas>();
 				}
             }
         }

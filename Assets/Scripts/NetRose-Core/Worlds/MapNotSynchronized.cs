859bd5578f7c7544c0e3dc278d18ceadc361db5c
Luis Masuelli <luisfmasuelli@gmail.com>
2020-08-15 20:56:16 -0500

    Adding NetworkedMap

---
+++ Assets/Scripts/NetRose-Core/Worlds/MapNotSynchronized.cs
@@ -0,0 +1,15 @@
+﻿namespace NetRose
+{
+    namespace Worlds
+    {
+        /// <summary>
+        ///   Triggered when the new map to attach a networked map object, has no networked map behaviour.
+        /// </summary>
+        class MapNotSynchronized : Exception
+        {
+            public MapNotSynchronized() { }
+            public MapNotSynchronized(string message) : base(message) { }
+            public MapNotSynchronized(string message, System.Exception inner) : base(message, inner) { }
+        }
+    }
+}
---
+++ Assets/Scripts/NetRose-Core/Worlds/NetworkedMap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Mirror;
+
+namespace NetRose
+{
+    namespace Worlds
+    {
+        using WindRose.Behaviours.World;
+        using WindRose.Behaviours.World.Layers.Objects;
+
+        /// <summary>
+        ///   Networked maps will force maps' <see cref="ObjectsManagementStrategyHolder"/>
+        ///     in objects layer to mark their <see cref="ObjectsManagementStrategyHolder.Bypass"/>
+        ///     to true (if server or host), or false (otherwise).
+        /// </summary>
+        [RequireComponent(typeof(Map))]
+        public class NetworkedMap : NetworkBehaviour
+        {
+            private void Start()
+            {
+                Map map = GetComponent<Map>();
+                if (!map) return;
+
+                map.ObjectsLayer.StrategyHolder.Bypass = !isServer;
+            }
+        }
+    }
+}

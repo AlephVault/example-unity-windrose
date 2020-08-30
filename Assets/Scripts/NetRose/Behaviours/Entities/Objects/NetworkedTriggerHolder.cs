6b56ddbab1c8520ffed797cbb6213ad5e8452775
Luis Masuelli <luisfmasuelli@gmail.com>
2020-08-30 00:05:13 -0500

    Using networked trigger holder which, essentially, disables the colliders in non-server-side

---
+++ Assets/Scripts/NetRose/Behaviours/Entities/Objects/NetworkedTriggerHolder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Mirror;
+
+namespace NetRose
+{
+    namespace Behaviours
+    {
+        namespace Entities
+        {
+            namespace Objects
+            {
+                using WindRose.Behaviours.Entities.Objects;
+
+                /// <summary>
+                ///   Networked trigger holders disable all the colliders of a behaviour,
+                ///     if the game is not server-side.
+                /// </summary>
+                [RequireComponent(typeof(TriggerHolder))]
+                public class NetworkedTriggerHolder : NetworkBehaviour
+                {
+                    public void Start()
+                    {
+                        if (!isServer)
+                        {
+                            foreach(Collider collider in GetComponents<Collider>())
+                            {
+                                collider.enabled = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

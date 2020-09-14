8cdb175f4c2a0b4902c1132a39562abd2d58c945
Luis Masuelli <luisfmasuelli@gmail.com>
2020-09-13 23:14:06 -0500

    Adding more classes

---
+++ Assets/Scripts/NetRose/Behaviours/Sessions/DuplicateAccountRule.cs
@@ -0,0 +1,20 @@
+﻿namespace NetRose
+{
+    namespace Behaviours
+    {
+        namespace Sessions
+        {
+            /// <summary>
+            ///   Tells what to do when an account logs in while
+            ///     it is currently logged in another connection.
+            ///     The "Kick" rule stands for kicking the new
+            ///     connection, while the "Ghost" rule stands
+            ///     for kicking the old connection.
+            /// </summary>
+            public enum DuplicateAccountRule
+            {
+                Kick, Ghost
+            }
+        }
+    }
+}

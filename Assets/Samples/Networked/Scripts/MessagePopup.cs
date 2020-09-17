bd03c17779a6e6f3f2aa0cb3d28b608bbdc84f77
Luis Masuelli <luisfmasuelli@gmail.com>
2020-09-16 21:37:22 -0500

    Reorganizing sample files

---
+++ Assets/Samples/Networked/Scripts/MessagePopup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GabTab.Behaviours;
+using TMPro;
+
+[RequireComponent(typeof(Hideable))]
+public class MessagePopup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hideable hideable = GetComponent<Hideable>();
+        Button button = GetComponent<Button>();
+        TMP_Text text = GetComponent<TMP_Text>();
+        button.onClick.AddListener(delegate ()
+        {
+            hideable.Hidden = true;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

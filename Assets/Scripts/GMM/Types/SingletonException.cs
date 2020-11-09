a72bfc4217bd74f5cfc5dd4f7b781cd29fa1266e
Luis Masuelli <luisfmasuelli@gmail.com>
2020-11-09 14:32:40 -0500

    Adding a singleton behaviour

---
+++ Assets/Scripts/GMM/Types/SingletonException.cs
@@ -0,0 +1,15 @@
+﻿namespace GMM
+{
+    namespace Types
+    {
+        /// <summary>
+        ///   An exception class for singletons.
+        /// </summary>
+        public class SingletonException : Exception
+        {
+            public SingletonException() { }
+            public SingletonException(string message) : base(message) { }
+            public SingletonException(string message, System.Exception inner) : base(message, inner) { }
+        }
+    }
+}

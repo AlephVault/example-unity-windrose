ff8cb8fd13b50952011b346d94184ea9fc82fb07
Luis Masuelli <luismasuelli@hotmail.com>
2021-07-25 22:22:55 -0500

    Adding an error class

---
+++ Assets/com.alephvault.unity.mmo/Runtime/Types/Exception.cs
@@ -0,0 +1,12 @@
+namespace AlephVault.Unity.MMO
+{
+    namespace Types
+    {
+        public class Exception : Support.Types.Exception
+        {
+            public Exception() { }
+            public Exception(string message) : base(message) { }
+            public Exception(string message, System.Exception inner) : base(message, inner) { }
+        }
+    }
+}

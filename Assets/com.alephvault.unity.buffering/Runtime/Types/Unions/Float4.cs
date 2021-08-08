47729087c7859cc5efc6967b078f2b7285fe1dcb
Luis Masuelli <luismasuelli@hotmail.com>
2021-08-08 00:51:43 -0500

    Renaming the networking package to buffering

---
+++ Assets/com.alephvault.unity.buffering/Runtime/Types/Unions/Float4.cs
@@ -0,0 +1,29 @@
+using System.Runtime.InteropServices;
+
+
+namespace AlephVault.Unity.Networking
+{
+    namespace Types
+    {
+        namespace Unions
+        {
+            /// <summary>
+            ///   <para>
+            ///     A 32-bit float that can be mapped into an uint value.
+            ///   </para>
+            ///   <para>
+            ///     Essentially stolen from MLAPI package.
+            ///   </para>
+            /// </summary>
+            [StructLayout(LayoutKind.Explicit)]
+            internal struct Float4
+            {
+                [FieldOffset(0)]
+                public float FloatValue;
+
+                [FieldOffset(0)]
+                public uint UIntValue;
+            }
+        }
+    }
+}

7ec3ea09c87645138a05afda9947a24ae35f85f0
Luis Masuelli <luismasuelli@hotmail.com>
2021-08-16 09:16:39 -0500

    Ensuring the protocol setup is invoked on client awake

--- Assets/com.alephvault.unity.meetgard/Runtime/Client/NetworkClient/NetworkClient_main.cs
+++ Assets/com.alephvault.unity.meetgard/Runtime/Client/NetworkClient/NetworkClient_main.cs
@@ -68,6 +68,7 @@ namespace AlephVault.Unity.Meetgard
             {
                 maxMessageSize = Values.Clamp(512, maxMessageSize, 6144);
                 idleSleepTime = Values.Clamp(0.005f, idleSleepTime, 0.5f);
+                SetupClientProtocols();
             }
 
             private void OnDestroy()

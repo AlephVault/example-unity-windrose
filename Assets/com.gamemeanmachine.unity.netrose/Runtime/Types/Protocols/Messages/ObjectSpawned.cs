c3b9e486e34d6efdeebd5fbcb1ca0aa846467fba
Luis Masuelli <luismasuelli@hotmail.com>
2021-09-11 09:20:52 -0500

    Moving two messages to a non-namespace folder

--- Assets/com.gamemeanmachine.unity.netrose/Runtime/Types/Protocols/Messages/ObjectSpawned.cs
+++ Assets/com.gamemeanmachine.unity.netrose/Runtime/Types/Protocols/Messages/ObjectSpawned.cs
@@ -5,13 +5,18 @@ namespace GameMeanMachine.Unity.NetRose
         namespace Protocols
         {
             using AlephVault.Unity.Binary;
+            using GameMeanMachine.Unity.WindRose.Types;
 
             /// <summary>
-            ///   This message tells the client that an object
-            ///   is being spawned in a scope the client belongs
-            ///   to (i.e. is connected right now).
+            ///   This message tells the client about a new/existing
+            ///   object in a scope they belong to. The new object
+            ///   will synchronize: scope index, object's prefab
+            ///   index, object's index, object's data, and whether
+            ///   the object is attached to a map, and which map
+            ///   and position, if true. Also, orientation and
+            ///   speed are synchronized.
             /// </summary>
-            public class ObjectSpawned<T> : ISerializable where T : ISerializable, new()
+            public class ExistingObject<T> : ISerializable where T : ISerializable, new()
             {
                 /// <summary>
                 ///   The server-side index of the scope the client
@@ -20,8 +25,7 @@ namespace GameMeanMachine.Unity.NetRose
                 public uint ScopeInstanceIndex;
 
                 /// <summary>
-                ///   The index of the prefab the object to spawn
-                ///   belongs to.
+                ///   The index of the prefab the object is made with.
                 /// </summary>
                 public uint ObjectPrefabIndex;
 
@@ -32,9 +36,44 @@ namespace GameMeanMachine.Unity.NetRose
                 public uint ObjectInstanceIndex;
 
                 /// <summary>
-                ///   The spawned object's data.
+                ///   The object's data.
                 /// </summary>
                 public T Data;
+
+                /// <summary>
+                ///   The object's orientation.
+                /// </summary>
+                public Direction Orientation;
+
+                /// <summary>
+                ///   The current speed, if the object is attached.
+                /// </summary>
+                public uint Speed;
+
+                /// <summary>
+                ///   Whether the object is attached to a map.
+                /// </summary>
+                public bool Attached;
+
+                /// <summary>
+                ///   The index of the map, if the object is attached.
+                /// </summary>
+                public byte MapIndex;
+
+                /// <summary>
+                ///   The X position, if the object is attached.
+                /// </summary>
+                public ushort X;
+
+                /// <summary>
+                ///   The Y position, if the object is attached.
+                /// </summary>
+                public ushort Y;
+
+                /// <summary>
+                ///   The current movement, if the object is attached.
+                /// </summary>
+                public Direction? Movement;
 
                 public void Serialize(Serializer serializer)
                 {
@@ -43,6 +82,38 @@ namespace GameMeanMachine.Unity.NetRose
                     serializer.Serialize(ref ObjectInstanceIndex);
                     Data = Data ?? new T();
                     Data.Serialize(serializer);
+                    serializer.Serialize(ref Orientation);
+                    serializer.Serialize(ref Speed);
+                    serializer.Serialize(ref Attached);
+                    if (Attached)
+                    {
+                        serializer.Serialize(ref MapIndex);
+                        serializer.Serialize(ref X);
+                        serializer.Serialize(ref Y);
+                        SerializeMovement(serializer);
+                    }
+                }
+
+                private void SerializeMovement(Serializer serializer)
+                {
+                    if (serializer.IsReading)
+                    {
+                        if (serializer.Reader.ReadBool()) {
+                            Direction movement = Direction.FRONT;
+                            serializer.Serialize(ref movement);
+                            Movement = movement;
+                        }
+                    }
+                    else
+                    {
+                        bool hasMovement = Movement.HasValue;
+                        serializer.Writer.WriteBool(hasMovement);
+                        if (hasMovement)
+                        {
+                            Direction movement = Movement.Value;
+                            serializer.Serialize(ref movement);
+                        }
+                    }
                 }
             }
         }

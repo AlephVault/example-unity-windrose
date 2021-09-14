80f851dd910d7e9228ff5a20e1776dd9afa19207
Luis Masuelli <luismasuelli@hotmail.com>
2021-09-14 00:02:04 -0500

    Moving NetRoseSetup into the Types namespace

--- Assets/com.gamemeanmachine.unity.netrose/Runtime/Types/NetRoseSetup.cs
+++ Assets/com.gamemeanmachine.unity.netrose/Runtime/Types/NetRoseSetup.cs
@@ -5,176 +5,179 @@ using System.Collections.Generic;
 
 namespace GameMeanMachine.Unity.NetRose
 {
-    /// <summary>
-    ///   This is a setup for the netrose protocol, which allows
-    ///   the netrose extensions developer to register custom
-    ///   types, statically, for the allowed synchronization
-    ///   messages. Concretely, this involves: 1. Custom spawn
-    ///   message types, 2. Custom property update types, 3.
-    ///   Custom "watch" (or "partial spawn" / "add-on") types,
-    ///   and 4. Custom property updates for the "watch" types.
-    /// </summary>
-    public static class NetRoseSetup
+    namespace Types
     {
-        // The registered primary models' sync type and properties.
-        private static Dictionary<Type, Tuple<Type, Dictionary<string, Type>>> mapObjectPrimaryModels = new Dictionary<Type, Tuple<Type, Dictionary<string, Type>>>();
-
-        // The registered watched models' sync type and properties.
-        private static Dictionary<Type, Tuple<Type, Dictionary<string, Type>>> mapObjectWatchedModels = new Dictionary<Type, Tuple<Type, Dictionary<string, Type>>>();
-
         /// <summary>
-        ///   Registers a <see cref="MapObjectPrimaryModel{T}"/>
-        ///   class in the netrose setup for the existing model
-        ///   classes (only primary ones are registered here).
+        ///   This is a setup for the netrose protocol, which allows
+        ///   the netrose extensions developer to register custom
+        ///   types, statically, for the allowed synchronization
+        ///   messages. Concretely, this involves: 1. Custom spawn
+        ///   message types, 2. Custom property update types, 3.
+        ///   Custom "watch" (or "partial spawn" / "add-on") types,
+        ///   and 4. Custom property updates for the "watch" types.
         /// </summary>
-        /// <typeparam name="ModelClass">The model class</typeparam>
-        /// <typeparam name="ModelType">The model class' sync type</typeparam>
-        /// <returns>Whether the model was just registered (returns false if it was already registered)</returns>
-        public static bool RegisterMapObjectPrimaryModel<ModelClass, ModelType>()
-            where ModelClass : MapObjectPrimaryModel<ModelType>
-            where ModelType : ISerializable, new()
+        public static class NetRoseSetup
         {
-            if (!mapObjectPrimaryModels.ContainsKey(typeof(ModelClass)))
-            {
-                mapObjectPrimaryModels.Add(typeof(ModelClass), new Tuple<Type, Dictionary<string, Type>>(typeof(ModelType), new Dictionary<string, Type>()));
-                return true;
-            }
-            return false;
-        }
+            // The registered primary models' sync type and properties.
+            private static Dictionary<Type, Tuple<Type, Dictionary<string, Type>>> mapObjectPrimaryModels = new Dictionary<Type, Tuple<Type, Dictionary<string, Type>>>();
 
-        /// <summary>
-        ///   Registers a <see cref="MapObjectPrimaryModel{T}"/>
-        ///   class in the netrose setup for the existing model
-        ///   classes (only watched ones are registered here).
-        /// </summary>
-        /// <typeparam name="ModelClass">The model class</typeparam>
-        /// <typeparam name="ModelType">The model class' sync type</typeparam>
-        /// <returns>Whether the model was just registered (returns false if it was already registered)</returns>
-        public static bool RegisterMapObjectWatchedModel<ModelClass, ModelType>()
-            where ModelClass : MapObjectWatchedModel<ModelType>
-            where ModelType : ISerializable, new()
-        {
-            if (!mapObjectPrimaryModels.ContainsKey(typeof(ModelClass)))
-            {
-                mapObjectPrimaryModels.Add(typeof(ModelClass), new Tuple<Type, Dictionary<string, Type>>(typeof(ModelType), new Dictionary<string, Type>()));
-                return true;
-            }
-            return false;
-        }
+            // The registered watched models' sync type and properties.
+            private static Dictionary<Type, Tuple<Type, Dictionary<string, Type>>> mapObjectWatchedModels = new Dictionary<Type, Tuple<Type, Dictionary<string, Type>>>();
 
-        /// <summary>
-        ///   Registers a property, positionally, for the
-        ///   given <see cref="MapObjectPrimaryModel{T}"/>
-        ///   type (which must be already registered) and
-        ///   a chosen property type.
-        /// </summary>
-        /// <typeparam name="ModelClass">The model class to register a property for</typeparam>
-        /// <typeparam name="PropertyType">The type of the property to register</typeparam>
-        /// <param name="propertyName">The name of the property</param>
-        /// <returns>Whether the property was just registered (returns false if it was already registered)</returns>
-        public static bool RegisterMapObjectPrimaryModelProperty<ModelClass, PropertyType>(string propertyName)
-            where PropertyType : ISerializable, new()
-        {
-            if (mapObjectPrimaryModels.TryGetValue(typeof(ModelClass), out var entry))
+            /// <summary>
+            ///   Registers a <see cref="MapObjectPrimaryModel{T}"/>
+            ///   class in the netrose setup for the existing model
+            ///   classes (only primary ones are registered here).
+            /// </summary>
+            /// <typeparam name="ModelClass">The model class</typeparam>
+            /// <typeparam name="ModelType">The model class' sync type</typeparam>
+            /// <returns>Whether the model was just registered (returns false if it was already registered)</returns>
+            public static bool RegisterMapObjectPrimaryModel<ModelClass, ModelType>()
+                where ModelClass : MapObjectPrimaryModel<ModelType>
+                where ModelType : ISerializable, new()
             {
-                if (!entry.Item2.ContainsKey(propertyName))
+                if (!mapObjectPrimaryModels.ContainsKey(typeof(ModelClass)))
                 {
-                    entry.Item2.Add(propertyName, typeof(PropertyType));
+                    mapObjectPrimaryModels.Add(typeof(ModelClass), new Tuple<Type, Dictionary<string, Type>>(typeof(ModelType), new Dictionary<string, Type>()));
                     return true;
                 }
                 return false;
             }
-            else
-            {
-                throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectPrimaryModel for it first");
-            }
-        }
 
-        /// <summary>
-        ///   Registers a property, positionally, for the
-        ///   given <see cref="MapObjectWatchedModel{T}"/>
-        ///   type (which must be already registered) and
-        ///   a chosen property type.
-        /// </summary>
-        /// <typeparam name="ModelClass">The model class to register a property for</typeparam>
-        /// <typeparam name="PropertyType">The type of the property to register</typeparam>
-        /// <param name="propertyName">The name of the property</param>
-        /// <returns>Whether the property was just registered (returns false if it was already registered)</returns>
-        public static bool RegisterMapObjectWatchedModelProperty<ModelClass, PropertyType>(string propertyName)
-            where PropertyType : ISerializable, new()
-        {
-            if (mapObjectWatchedModels.TryGetValue(typeof(ModelClass), out var entry))
+            /// <summary>
+            ///   Registers a <see cref="MapObjectPrimaryModel{T}"/>
+            ///   class in the netrose setup for the existing model
+            ///   classes (only watched ones are registered here).
+            /// </summary>
+            /// <typeparam name="ModelClass">The model class</typeparam>
+            /// <typeparam name="ModelType">The model class' sync type</typeparam>
+            /// <returns>Whether the model was just registered (returns false if it was already registered)</returns>
+            public static bool RegisterMapObjectWatchedModel<ModelClass, ModelType>()
+                where ModelClass : MapObjectWatchedModel<ModelType>
+                where ModelType : ISerializable, new()
             {
-                if (!entry.Item2.ContainsKey(propertyName))
+                if (!mapObjectPrimaryModels.ContainsKey(typeof(ModelClass)))
                 {
-                    entry.Item2.Add(propertyName, typeof(PropertyType));
+                    mapObjectPrimaryModels.Add(typeof(ModelClass), new Tuple<Type, Dictionary<string, Type>>(typeof(ModelType), new Dictionary<string, Type>()));
                     return true;
                 }
                 return false;
             }
-            else
-            {
-                throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectPrimaryModel for it first");
-            }
-        }
 
-        /// <summary>
-        ///   Given a particular type (which is a subclass
-        ///   of <see cref="MapObjectPrimaryModel{T}"/>),
-        ///   this method retrieves it and also enumerates
-        ///   its registered properties. The class itself
-        ///   is processed inside a callback, and each of
-        ///   its registered properties are also processed
-        ///   inside another callback (passing both the
-        ///   index and the type to it). If the type is not
-        ///   registered, an error is thrown.
-        /// </summary>
-        /// <typeparam name="ModelClass">The registered model class to act upon</typeparam>
-        /// <param name="modelClassCallback">A callback to act upon the sync type</param>
-        /// <param name="modelPropertyCallback">A callback to act upon each registered property</param>
-        public static void WithMapObjectPrimaryModelType<ModelClass>(Action<Type> modelClassCallback, Action<string, Type> modelPropertyCallback)
-        {
-            if (mapObjectPrimaryModels.TryGetValue(typeof(ModelClass), out var entry))
+            /// <summary>
+            ///   Registers a property, positionally, for the
+            ///   given <see cref="MapObjectPrimaryModel{T}"/>
+            ///   type (which must be already registered) and
+            ///   a chosen property type.
+            /// </summary>
+            /// <typeparam name="ModelClass">The model class to register a property for</typeparam>
+            /// <typeparam name="PropertyType">The type of the property to register</typeparam>
+            /// <param name="propertyName">The name of the property</param>
+            /// <returns>Whether the property was just registered (returns false if it was already registered)</returns>
+            public static bool RegisterMapObjectPrimaryModelProperty<ModelClass, PropertyType>(string propertyName)
+                where PropertyType : ISerializable, new()
             {
-                modelClassCallback?.Invoke(entry.Item1);
-                foreach(KeyValuePair<string, Type> pair in entry.Item2)
+                if (mapObjectPrimaryModels.TryGetValue(typeof(ModelClass), out var entry))
                 {
-                    modelPropertyCallback?.Invoke(pair.Key, pair.Value);
+                    if (!entry.Item2.ContainsKey(propertyName))
+                    {
+                        entry.Item2.Add(propertyName, typeof(PropertyType));
+                        return true;
+                    }
+                    return false;
+                }
+                else
+                {
+                    throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectPrimaryModel for it first");
                 }
             }
-            else
-            {
-                throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectPrimaryModel for it first");
-            }
-        }
 
-        /// <summary>
-        ///   Given a particular type (which is a subclass
-        ///   of <see cref="MapObjectWatchedModel{T}"/>),
-        ///   this method retrieves it and also enumerates
-        ///   its registered properties. The class itself
-        ///   is processed inside a callback, and each of
-        ///   its registered properties are also processed
-        ///   inside another callback (passing both the
-        ///   index and the type to it). If the type is not
-        ///   registered, an error is thrown.
-        /// </summary>
-        /// <typeparam name="ModelClass">The registered model class to act upon</typeparam>
-        /// <param name="modelClassCallback">A callback to act upon the sync type</param>
-        /// <param name="modelPropertyCallback">A callback to act upon each registered property</param>
-        public static void WithMapObjectWatchedModelType<ModelClass>(Action<Type> modelClassCallback, Action<string, Type> modelPropertyCallback)
-        {
-            if (mapObjectWatchedModels.TryGetValue(typeof(ModelClass), out var entry))
+            /// <summary>
+            ///   Registers a property, positionally, for the
+            ///   given <see cref="MapObjectWatchedModel{T}"/>
+            ///   type (which must be already registered) and
+            ///   a chosen property type.
+            /// </summary>
+            /// <typeparam name="ModelClass">The model class to register a property for</typeparam>
+            /// <typeparam name="PropertyType">The type of the property to register</typeparam>
+            /// <param name="propertyName">The name of the property</param>
+            /// <returns>Whether the property was just registered (returns false if it was already registered)</returns>
+            public static bool RegisterMapObjectWatchedModelProperty<ModelClass, PropertyType>(string propertyName)
+                where PropertyType : ISerializable, new()
             {
-                modelClassCallback?.Invoke(entry.Item1);
-                foreach (KeyValuePair<string, Type> pair in entry.Item2)
+                if (mapObjectWatchedModels.TryGetValue(typeof(ModelClass), out var entry))
                 {
-                    modelPropertyCallback?.Invoke(pair.Key, pair.Value);
+                    if (!entry.Item2.ContainsKey(propertyName))
+                    {
+                        entry.Item2.Add(propertyName, typeof(PropertyType));
+                        return true;
+                    }
+                    return false;
+                }
+                else
+                {
+                    throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectPrimaryModel for it first");
                 }
             }
-            else
+
+            /// <summary>
+            ///   Given a particular type (which is a subclass
+            ///   of <see cref="MapObjectPrimaryModel{T}"/>),
+            ///   this method retrieves it and also enumerates
+            ///   its registered properties. The class itself
+            ///   is processed inside a callback, and each of
+            ///   its registered properties are also processed
+            ///   inside another callback (passing both the
+            ///   index and the type to it). If the type is not
+            ///   registered, an error is thrown.
+            /// </summary>
+            /// <typeparam name="ModelClass">The registered model class to act upon</typeparam>
+            /// <param name="modelClassCallback">A callback to act upon the sync type</param>
+            /// <param name="modelPropertyCallback">A callback to act upon each registered property</param>
+            public static void WithMapObjectPrimaryModelType<ModelClass>(Action<Type> modelClassCallback, Action<string, Type> modelPropertyCallback)
             {
-                throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectWatchedModel for it first");
+                if (mapObjectPrimaryModels.TryGetValue(typeof(ModelClass), out var entry))
+                {
+                    modelClassCallback?.Invoke(entry.Item1);
+                    foreach (KeyValuePair<string, Type> pair in entry.Item2)
+                    {
+                        modelPropertyCallback?.Invoke(pair.Key, pair.Value);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectPrimaryModel for it first");
+                }
+            }
+
+            /// <summary>
+            ///   Given a particular type (which is a subclass
+            ///   of <see cref="MapObjectWatchedModel{T}"/>),
+            ///   this method retrieves it and also enumerates
+            ///   its registered properties. The class itself
+            ///   is processed inside a callback, and each of
+            ///   its registered properties are also processed
+            ///   inside another callback (passing both the
+            ///   index and the type to it). If the type is not
+            ///   registered, an error is thrown.
+            /// </summary>
+            /// <typeparam name="ModelClass">The registered model class to act upon</typeparam>
+            /// <param name="modelClassCallback">A callback to act upon the sync type</param>
+            /// <param name="modelPropertyCallback">A callback to act upon each registered property</param>
+            public static void WithMapObjectWatchedModelType<ModelClass>(Action<Type> modelClassCallback, Action<string, Type> modelPropertyCallback)
+            {
+                if (mapObjectWatchedModels.TryGetValue(typeof(ModelClass), out var entry))
+                {
+                    modelClassCallback?.Invoke(entry.Item1);
+                    foreach (KeyValuePair<string, Type> pair in entry.Item2)
+                    {
+                        modelPropertyCallback?.Invoke(pair.Key, pair.Value);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("The chosen ModelClass type is not registered - Ensure you call RegisterMapObjectWatchedModel for it first");
+                }
             }
         }
     }

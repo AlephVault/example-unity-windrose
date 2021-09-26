2d579257b73572508267aab56ee934040d1d8ca8
Luis Masuelli <luismasuelli@hotmail.com>
2021-09-26 09:26:44 -0500

    Adding the Primary/Watched models, which were missing but are required for the model server sides to exist

---
+++ Assets/com.gamemeanmachine.unity.netrose/Runtime/Authoring/Behaviours/Models/Entities/Objects/MapObjectWatchedModel.cs
@@ -0,0 +1,40 @@
+using AlephVault.Unity.Binary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameMeanMachine.Unity.NetRose
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Models
+            {
+                namespace Entities.Objects
+                {
+                    /// <summary>
+                    ///   This behaviours stands for a model holder
+                    ///   involving a particular non-public view
+                    ///   over an object (e.g. its inventory, or
+                    ///   its health status).
+                    /// </summary>
+                    /// <typeparam name="T">The underlying model type</typeparam>
+                    [RequireComponent(typeof(MapObject))]
+                    public abstract class MapObjectWatchedModel<T> : MonoBehaviour where T : ISerializable, new()
+                    {
+                        // The internal, watched, model data.
+                        private T data = new T();
+
+                        /// <summary>
+                        ///   A read-only access to the whole data.
+                        /// </summary>
+                        public T FullData => data;
+                    }
+                }
+            }
+        }
+    }
+}

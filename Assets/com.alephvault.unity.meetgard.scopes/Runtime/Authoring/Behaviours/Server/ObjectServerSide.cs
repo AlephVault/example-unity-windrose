d33bacd1b4bbb597529f4239d79639465c0e3f27
Luis Masuelli <luis@datagran.io>
2021-10-14 17:41:52 -0500

    Testing push from Ubuntu

---
+++ Assets/com.alephvault.unity.meetgard.scopes/Runtime/Authoring/Behaviours/Server/ObjectServerSide.cs
@@ -0,0 +1,23 @@
+using AlephVault.Unity.Meetgard.Scopes.Types.Constants;
+using AlephVault.Unity.Support.Authoring.Behaviours;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace AlephVault.Unity.Meetgard.Scopes
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Server
+            {
+                public class ObjectServerSide : MonoBehaviour
+                {
+                    // TODO implement
+                }
+            }
+        }
+    }
+}

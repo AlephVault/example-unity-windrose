dcb3f6d8a6f0a3a7896187a9207ca60cac4fbe15
Luis Masuelli <luismasuelli@hotmail.com>
2021-10-16 22:45:33 -0500

    Adding the implementation of some object-related methods and events

---
+++ Assets/com.alephvault.unity.meetgard.scopes/Runtime/Authoring/Behaviours/Server/ScopeServerSide_objects.cs
@@ -0,0 +1,206 @@
+using AlephVault.Unity.Binary;
+using AlephVault.Unity.Meetgard.Scopes.Types.Constants;
+using AlephVault.Unity.Meetgard.Scopes.Types.Protocols.Messages;
+using AlephVault.Unity.Support.Types;
+using AlephVault.Unity.Support.Utils;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace AlephVault.Unity.Meetgard.Scopes
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Server
+            {
+                public partial class ScopeServerSide : MonoBehaviour
+                {
+                    // This is the list of objects registered in this
+                    // scope. The id given to them is scope-specific.
+                    private Dictionary<uint, ObjectServerSide> objects = new Dictionary<uint, ObjectServerSide>();
+
+                    // This is the list of IDs registered in this scope.
+                    private IdPool objectIds = new IdPool(uint.MaxValue);
+
+                    // This is the array to send custom object data.
+                    private byte[] fullObjectData;
+
+                    /// <summary>
+                    ///   Triggered when an object is spawned inside the
+                    ///   current scope.
+                    /// </summary>
+                    public event Func<ObjectServerSide, Task> OnSpawned = null;
+
+                    /// <summary>
+                    ///   Triggered when an object is despawned from the
+                    ///   last scope. By this point, the object will not
+                    ///   have any sort of scope association information.
+                    /// </summary>
+                    public event Func<ObjectServerSide, Task> OnDespawned = null;
+
+                    // Registers a new object in this scope.
+                    private void RegisterObject(ObjectServerSide target)
+                    {
+                        uint newId = (uint)objectIds.Next();
+                        target.Id = newId;
+                        target.Scope = this;
+                        objects.Add(newId, target);
+                    }
+
+                    // Unregisters an object from this scope.
+                    private void UnregisterObject(ObjectServerSide target)
+                    {
+                        objectIds.Release(target.Id);
+                        objects.Remove(target.Id);
+                        target.Id = 0;
+                        target.Scope = null;
+                    }
+
+                    // Broadcasts the object's data to all of the available
+                    // connections in the scope.
+                    private async Task NotifyObjectSpawnedToEveryone(ObjectServerSide target)
+                    {
+                        foreach(Tuple<HashSet<ulong>, ISerializable> pair in target.FullData(connections))
+                        {
+                            if (pair.Item2 == null)
+                            {
+                                Debug.LogWarning("Trying to spawn using a null value as data - the spawn was ignored");
+                                continue;
+                            }
+
+                            // Lazy-allocate the array.
+                            if (fullObjectData == null) fullObjectData = Protocol.AllocateFullDataMessageBytes();
+                            // Serialize the object into the array.
+                            pair.Item2.Serialize(new Serializer(new Writer(new Binary.Buffer(fullObjectData))));                            
+                            // Wait for all the sends.
+                            await Tasks.UntilAllDone(Protocol.BroadcastObjectSpawned(pair.Item1, new ObjectSpawned() {
+                                ObjectIndex = target.Id,
+                                ObjectPrefabIndex = target.PrefabId,
+                                ScopeIndex = Id,
+                                Data = fullObjectData
+                            }).Values);
+                        }
+                    }
+
+                    // Broadcasts the object disappearance from the scope.
+                    private async Task NotifyObjectDespawnedToEveryone(ObjectServerSide target)
+                    {
+                        await Tasks.UntilAllDone(Protocol.BroadcastObjectDespawned(connections, new ObjectDespawned() {
+                            ObjectIndex = target.Id,
+                            ScopeIndex = Id
+                        }).Values);
+                    }
+
+                    /// <summary>
+                    ///   <para>
+                    ///     Adds an object to the current scope. If the object
+                    ///     is already added to a scope, this is an error. If
+                    ///     the object belongs to a different server this scope
+                    ///     belongs to, it is an error. This task is queued in
+                    ///     the current queue.
+                    ///   </para>
+                    ///   <para>
+                    ///     This method can be called standalone or will be called
+                    ///     from the object itself, on hierarchy change.
+                    ///   </para>
+                    /// </summary>
+                    /// <param name="obj">The object to add</param>
+                    public Task AddObject(ObjectServerSide target)
+                    {
+                        return queueManager.QueueTask(async () =>
+                        {
+                            // Null/Destroyed objects cannot be added.
+                            if (target == null) throw new ArgumentNullException("target");
+
+                            // Ignore if the scope is the same - the object is already
+                            // added to this scope.
+                            if (target.Scope == this) return;
+
+                            // Objects belonging to other scopes cannot be added.
+                            if (target.Scope != null) throw new ArgumentException("The target to add already belongs to a scope");
+
+                            // Then we force the object to be descendant of this scope.
+                            if (target.GetComponentInParent<ScopeServerSide>() != this)
+                            {
+                                target.transform.SetParent(transform);
+                            }
+
+                            // Finally, register the object, and broadcast.
+                            RegisterObject(target);
+                            await NotifyObjectSpawnedToEveryone(target);
+
+                            // In the end, trigger the event for the object.
+                            // TODO: implement a default handler, which goes
+                            // TODO: for this behaviour.
+                            await OnSpawned?.InvokeAsync(target);
+                        });
+                    }
+
+                    /// <summary>
+                    ///   <para>
+                    ///     Removes an object from the current scope. If the
+                    ///     object is not added to this scope, this is an error.
+                    ///     This task is queued in the current queue.
+                    ///   </para>
+                    ///   <para>
+                    ///     This method can be called standalone or will be called
+                    ///     from the object itself, on hierarchy change.
+                    ///   </para>
+                    /// </summary>
+                    /// <param name="obj">The object to remove</param>
+                    public Task RemoveObject(ObjectServerSide target)
+                    {
+                        return queueManager.QueueTask(async () =>
+                        {
+                            // Null objects cannot be removed.
+                            if (ReferenceEquals(target, null)) throw new ArgumentNullException("target");
+
+                            // Objects not belonging to this scope cannot be removed.
+                            if (target.Scope != null) throw new ArgumentException("The target to remove does not belong to this scope");
+
+                            // Then we force the object to be not descendant of this object.
+                            if (target.GetComponentInParent<ScopeServerSide>() == this)
+                            {
+                                target.transform.SetParent(transform.parent);
+                            }
+
+                            // Finally, unregister the object, and broadcast.
+                            await NotifyObjectDespawnedToEveryone(target);
+                            UnregisterObject(target);
+
+                            // In the end, trigger the event for the object.
+                            // TODO: implement a default handler, which goes
+                            // TODO: for this behaviour.
+                            await OnDespawned?.InvokeAsync(target);
+                        });
+                    }
+
+                    // Synchronizes all the existing objects to the connection.
+                    // This synchronization is initial, and will be part of a
+                    // bigger queued task.
+                    private async Task SyncExistingObjectsTo(ulong connection)
+                    {
+                        foreach(ObjectServerSide obj in objects.Values)
+                        {
+                            // Lazy-allocate the array.
+                            if (fullObjectData == null) fullObjectData = Protocol.AllocateFullDataMessageBytes();
+                            // Serialize the object into the array.
+                            obj.FullData(connection).Serialize(new Serializer(new Writer(new Binary.Buffer(fullObjectData))));
+                            await Tasks.UntilDone(Protocol.SendObjectSpawned(connection, new ObjectSpawned()
+                            {
+                                ObjectIndex = obj.Id,
+                                ObjectPrefabIndex = obj.PrefabId,
+                                ScopeIndex = Id,
+                                Data = fullObjectData
+                            }));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

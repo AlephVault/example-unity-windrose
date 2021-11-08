8370c017e444d1fb8d0ac6f37da6b5beeb56c704
Luis Masuelli <luismasuelli@hotmail.com>
2021-11-07 21:15:33 -0500

    Adding bare implementations of NetRose client and server objects

---
+++ Assets/com.gamemeanmachine.unity.netrose/Runtime/Authoring/Behaviours/Client/NetRoseMapObjectClientSide.cs
@@ -0,0 +1,29 @@
+﻿using AlephVault.Unity.Meetgard.Scopes.Authoring.Behaviours.Client;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Objects;
+using UnityEngine;
+
+
+namespace GameMeanMachine.Unity.NetRose
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Client
+            {
+                /// <summary>
+                ///   Map objects exist in client side, reflecting what is received
+                ///   from the server side, and applying some internal mechanisms
+                ///   for lag balance and recovery. These ones are also related to
+                ///   a single WindRose map object in a single map.
+                /// </summary>
+                [RequireComponent(typeof(ObjectClientSide))]
+                [RequireComponent(typeof(MapObject))]
+                public class NetRoseMapObjectClientSide : MonoBehaviour
+                {
+                    // TODO implement.
+                }
+            }
+        }
+    }
+}
---
+++ Assets/com.gamemeanmachine.unity.netrose/Runtime/Authoring/Behaviours/Server/NetRoseMapObjectServerSide.cs
@@ -0,0 +1,29 @@
+﻿using AlephVault.Unity.Meetgard.Scopes.Authoring.Behaviours.Server;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Objects;
+using UnityEngine;
+
+
+namespace GameMeanMachine.Unity.NetRose
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Server
+            {
+                /// <summary>
+                ///   Map objects exist in server side, reflecting what is changed
+                ///   in them to the client side, and sending those changes to the
+                ///   client side by means of the current scope. These ones are also
+                ///   related to a single WindRose map object in a single map.
+                /// </summary>
+                [RequireComponent(typeof(ObjectServerSide))]
+                [RequireComponent(typeof(MapObject))]
+                public class NetRoseMapObjectServerSide : MonoBehaviour
+                {
+                    // TODO implement.
+                }
+            }
+        }
+    }
+}

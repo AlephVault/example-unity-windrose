81bda5dd6e54f1d5c3bf8d6fcc602bd1a00dbceb
Luis Masuelli <luismasuelli@hotmail.com>
2021-11-08 23:29:03 -0500

    Adding the stub for the movement / speed / orientation commands

--- Assets/com.gamemeanmachine.unity.netrose/Runtime/Authoring/Behaviours/Client/NetRoseMapObjectClientSide_queue.cs
+++ Assets/com.gamemeanmachine.unity.netrose/Runtime/Authoring/Behaviours/Client/NetRoseMapObjectClientSide_queue.cs
@@ -23,6 +23,70 @@ namespace GameMeanMachine.Unity.NetRose
                         public abstract void Execute(bool accelerate);
                     }
 
+                    // A Movement Start queued command.
+                    private class MovementStartCommand : QueuedCommand
+                    {
+                        public ushort StartX;
+                        public ushort StartY;
+                        public Direction Direction;
+
+                        public override void Execute(bool accelerate)
+                        {
+                            // TODO implement.
+                            throw new System.NotImplementedException();
+                        }
+                    }
+
+                    // A Movement Cancel queued command.
+                    private class MovementCancelCommand : QueuedCommand
+                    {
+                        public ushort RevertX;
+                        public ushort RevertY;
+
+                        public override void Execute(bool accelerate)
+                        {
+                            // TODO implement.
+                            throw new System.NotImplementedException();
+                        }
+                    }
+
+                    // A Movement Finish queued command.
+                    private class MovementFinishCommand : QueuedCommand
+                    {
+                        public ushort EndX;
+                        public ushort EndY;
+
+                        public override void Execute(bool accelerate)
+                        {
+                            // TODO implement.
+                            throw new System.NotImplementedException();
+                        }
+                    }
+
+                    // A Speed Change queued command.
+                    private class SpeedChangeCommand : QueuedCommand
+                    {
+                        public uint Speed;
+
+                        public override void Execute(bool accelerate)
+                        {
+                            // TODO implement.
+                            throw new System.NotImplementedException();
+                        }
+                    }
+
+                    // An Orientation Change queued command.
+                    private class OrientationChangeCommand : QueuedCommand
+                    {
+                        public Direction Orientation;
+
+                        public override void Execute(bool accelerate)
+                        {
+                            // TODO implement.
+                            throw new System.NotImplementedException();
+                        }
+                    }
+
                     // Clears the queue.
                     private void ClearQueue()
                     {

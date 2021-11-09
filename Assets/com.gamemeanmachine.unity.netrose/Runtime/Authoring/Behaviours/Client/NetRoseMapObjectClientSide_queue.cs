7e0d526464e6ff01fe775a4d99c30724ea0256be
Luis Masuelli <luismasuelli@hotmail.com>
2021-11-08 23:21:45 -0500

    Adding a not fully implemented partial queue file

---
+++ Assets/com.gamemeanmachine.unity.netrose/Runtime/Authoring/Behaviours/Client/NetRoseMapObjectClientSide_queue.cs
@@ -0,0 +1,35 @@
+﻿using AlephVault.Unity.Meetgard.Scopes.Authoring.Behaviours.Client;
+using GameMeanMachine.Unity.NetRose.Types.Models;
+using GameMeanMachine.Unity.NetRose.Types.Protocols.Messages;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Objects;
+using GameMeanMachine.Unity.WindRose.Types;
+using UnityEngine;
+
+
+namespace GameMeanMachine.Unity.NetRose
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            namespace Client
+            {
+                public partial class NetRoseMapObjectClientSide : MonoBehaviour
+                {
+                    // One of the queued commands: Movement Start, Finish, Cancel,
+                    // Speed Change, and Orientation Change.
+                    private abstract class QueuedCommand
+                    {
+                        public abstract void Execute(bool accelerate);
+                    }
+
+                    // Clears the queue.
+                    private void ClearQueue()
+                    {
+                        // TODO implement.
+                    }
+                }
+            }
+        }
+    }
+}

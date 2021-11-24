9034eac3c888c75f8ce28b55c63da58cefeba71c
Luis Masuelli <luis@datagran.io>
2021-11-24 15:42:19 -0500

    Adding a docstring to the MissingBiomeSetException.

--- Assets/com.gamemeanmachine.unity.windrose.biomes/Runtime/Types/MissingBiomeSetException.cs
+++ Assets/com.gamemeanmachine.unity.windrose.biomes/Runtime/Types/MissingBiomeSetException.cs
@@ -5,6 +5,10 @@ namespace GameMeanMachine.Unity.WindRose.Biomes
 {
     namespace Types
     {
+        /// <summary>
+        ///   This exception is triggered when a biome is not set on behaviours
+        ///   that make use of them.
+        /// </summary>
         public class MissingBiomeSetException : Exception
         {
             public MissingBiomeSetException() { }

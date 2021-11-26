8d32f6aa18d49344d14712f38482a1cbdbb3dbe2
Luis Masuelli <luis@datagran.io>
2021-11-25 22:41:44 -0500

    Adding a custom editor for the biome object strategy

--- Assets/com.gamemeanmachine.unity.windrose.biomes/Runtime/Authoring/Behaviours/Entities/Objects/Strategies/BiomeObjectStrategy.cs
+++ Assets/com.gamemeanmachine.unity.windrose.biomes/Runtime/Authoring/Behaviours/Entities/Objects/Strategies/BiomeObjectStrategy.cs
@@ -1,4 +1,5 @@
 using System;
+using AlephVault.Unity.Support.Utils;
 using Codice.CM.Common.Serialization;
 using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Objects.Strategies;
 using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Objects.Strategies.Base;
@@ -7,6 +8,9 @@ using GameMeanMachine.Unity.WindRose.Biomes.Authoring.Behaviours.World.Layers.Ob
 using GameMeanMachine.Unity.WindRose.Biomes.Authoring.ScriptableObjects.Tiles;
 using GameMeanMachine.Unity.WindRose.Biomes.Types;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 
 namespace GameMeanMachine.Unity.WindRose.Biomes
@@ -25,6 +29,77 @@ namespace GameMeanMachine.Unity.WindRose.Biomes
                     [RequireComponent(typeof(LayoutObjectStrategy))]
                     public class BiomeObjectStrategy : ObjectStrategy
                     {
+                        #if UNITY_EDITOR
+                        [CustomEditor(typeof(BiomeObjectStrategy))]
+                        public class BiomeObjectStrategyEditor : Editor
+                        {
+                            // The biome tile strategy being edited.
+                            private BiomeObjectStrategy component;
+
+                            // The biome indices.
+                            private int[] indices;
+                            
+                            // The biome captions.
+                            private string[] captions;
+
+                            private void OnEnable()
+                            {
+                                component = (BiomeObjectStrategy)target;
+                                RefreshOptions();
+                            }
+
+                            private void RefreshOptions()
+                            {
+                                if (component.biomeSet != null)
+                                {
+                                    int index = 0;
+                                    indices = new int[component.biomeSet.Count];
+                                    captions = new string[component.biomeSet.Count];
+                                    foreach (Tuple<string, string> entry in component.biomeSet)
+                                    {
+                                        indices[index] = index;
+                                        captions[index] = $"{entry.Item2} ({entry.Item1})";
+                                        index++;
+                                    }
+                                }
+                                else
+                                {
+                                    indices = null;
+                                    captions = null;
+                                }
+                            }
+
+                            public override void OnInspectorGUI()
+                            {
+                                if (!component) return;
+                                
+                                component.biomeSet = (BiomeSet) EditorGUILayout.ObjectField(
+                                    "Biome Set", component.biomeSet,
+                                    typeof(BiomeSet), component
+                                );
+                                RefreshOptions();
+                                if (component.biomeSet)
+                                {
+                                    component.biome = (byte)Values.Clamp(
+                                        0, EditorGUILayout.IntPopup(
+                                            "Biome", component.biome, captions, indices
+                                        ),
+                                        component.biomeSet.Count - 1
+                                    );
+                                }
+                                else
+                                {
+                                    EditorGUILayout.HelpBox(
+                                        "Please select a biome set. Otherwise, this strategy won't work",
+                                        MessageType.Warning
+                                    );
+                                }
+                                
+                                EditorUtility.SetDirty(component);
+                            }
+                        }
+                        #endif
+                        
                         /// <summary>
                         ///   The counterpart type is <see cref="BiomeObjectsManagementStrategy"/>.
                         /// </summary>

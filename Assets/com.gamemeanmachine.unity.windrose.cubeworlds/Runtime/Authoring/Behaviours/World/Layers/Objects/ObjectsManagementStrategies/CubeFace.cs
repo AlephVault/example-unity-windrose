6d2eb875141a15b45f3842fb4a9f9a9fdf412081
Luis Masuelli <luis@datagran.io>
2021-12-11 16:44:16 -0500

    Removing some namespaces

--- Assets/com.gamemeanmachine.unity.windrose.cubeworlds/Runtime/Authoring/Behaviours/World/Layers/Objects/ObjectsManagementStrategies/CubeFace.cs
+++ Assets/com.gamemeanmachine.unity.windrose.cubeworlds/Runtime/Authoring/Behaviours/World/Layers/Objects/ObjectsManagementStrategies/CubeFace.cs
@@ -1,6 +1,5 @@
 using GameMeanMachine.Unity.WindRose.NeighbourTeleports.Authoring.Behaviours.World.Layers.Objects.ObjectsManagementStrategies;
 using GameMeanMachine.Unity.WindRose.CubeWorlds.Types;
-using GameMeanMachine.Unity.WindRose.CubeWorlds.Authoring.Behaviours.World;
 using UnityEngine;
 
 
@@ -12,39 +11,30 @@ namespace GameMeanMachine.Unity.WindRose.CubeWorlds
         {
             namespace World
             {
-                namespace Layers
+                /// <summary>
+                ///   Depends on neighbour teleport strategy and also behaves
+                ///   as a cube face (so it reorients appropriately). It also
+                ///   serves to <see cref="CubePivot"/>, which takes 6 cube
+                ///   faces and assembles them into a cube.
+                /// </summary>
+                [RequireComponent(typeof(NeighbourTeleportObjectsManagementStrategy))]
+                public class CubeFace : MonoBehaviour
                 {
-                    namespace Objects
+                    /// <summary>
+                    ///   The face orientation for this map.
+                    /// </summary>
+                    [SerializeField]
+                    private FaceOrientation faceOrientation;
+
+                    /// <summary>
+                    ///   See <see cref="faceOrientation"/>.
+                    /// </summary>
+                    public FaceOrientation FaceOrientation => faceOrientation;
+
+                    // Sets the local rotation of the map.
+                    protected void Awake()
                     {
-                        namespace ObjectsManagementStrategies
-                        {
-                            /// <summary>
-                            ///   Depends on neighbour teleport strategy and also behaves
-                            ///   as a cube face (so it reorients appropriately). It also
-                            ///   serves to <see cref="CubePivot"/>, which takes 6 cube
-                            ///   faces and assembles them into a cube.
-                            /// </summary>
-                            [RequireComponent(typeof(NeighbourTeleportObjectsManagementStrategy))]
-                            public class CubeFace : MonoBehaviour
-                            {
-                                /// <summary>
-                                ///   The face orientation for this map.
-                                /// </summary>
-                                [SerializeField]
-                                private FaceOrientation faceOrientation;
-
-                                /// <summary>
-                                ///   See <see cref="faceOrientation"/>.
-                                /// </summary>
-                                public FaceOrientation FaceOrientation => faceOrientation;
-
-                                // Sets the local rotation of the map.
-                                protected void Awake()
-                                {
-                                    transform.localRotation = FaceOrientation.Rotation();
-                                }
-                            }
-                        }
+                        transform.localRotation = FaceOrientation.Rotation();
                     }
                 }
             }

9a7f3d0325008301a2a52db4c86f844f88f2a1ad
Luis Masuelli <luis@datagran.io>
2021-12-23 09:57:42 -0500

    Adding the sample movement behaviour and slightly modifying the character in the scene.

--- Assets/com.gamemeanmachine.unity.windrose.cubeworlds/Samples/Scripts/SampleCubeWatcherAwareCharacter.cs
+++ Assets/com.gamemeanmachine.unity.windrose.cubeworlds/Samples/Scripts/SampleCubeWatcherAwareCharacter.cs
@@ -1,4 +1,6 @@
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Objects;
 using GameMeanMachine.Unity.WindRose.CubeWorlds.Authoring.Behaviours.Entities;
+using GameMeanMachine.Unity.WindRose.Types;
 using UnityEngine;
 
 
@@ -10,15 +12,37 @@ namespace GameMeanMachine.Unity.WindRose.CubeWorlds
         public class SampleCubeWatcherAwareCharacter : MonoBehaviour
         {
             // Start is called before the first frame update
-            void Start()
+            private MapObject obj;
+
+            // Start is called before the first frame update
+            void Awake()
             {
-        
+                obj = GetComponent<MapObject>();
             }
 
             // Update is called once per frame
             void Update()
             {
-        
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    obj.Orientation = Direction.DOWN;
+                    obj.StartMovement(Direction.DOWN);
+                }
+                else if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    obj.Orientation = Direction.UP;
+                    obj.StartMovement(Direction.UP);
+                }
+                else if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    obj.Orientation = Direction.LEFT;
+                    obj.StartMovement(Direction.LEFT);
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    obj.Orientation = Direction.RIGHT;
+                    obj.StartMovement(Direction.RIGHT);
+                }
             }
         }
     }

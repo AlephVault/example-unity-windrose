6e0ca8f40eba3140eeeb15a5367432e4cec9b014
Luis Masuelli <luis@datagran.io>
2022-02-24 11:22:01 -0500

    Adding the .meta files

---
+++ Assets/com.alephvault.unity.bundles/Runtime/Types/Bundle.cs
@@ -0,0 +1,26 @@
+namespace AlephVault.Unity.Bundles
+{
+    namespace Types
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public class Bundle
+        {
+            /// <summary>
+            ///   Loads a bundle from disk.
+            /// </summary>
+            /// <param name="directory"></param>
+            /// <returns></returns>
+            public static Bundle Load(string directory)
+            {
+                return null;
+            }
+            
+            // What I can do in a bundle is to retrieve a
+            // particular resource, like this:
+            //
+            // 
+        }
+    }
+}

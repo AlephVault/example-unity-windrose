1d0198b17b3e1d5f5db8c46665d9c8198381068f
Luis Masuelli <luis@datagran.io>
2022-03-16 19:23:54 -0500

    Completing the multi-state sprited and rose-sprited appliers.

--- Assets/com.gamemeanmachine.unity.windrose-spriteutils/Runtime/Authoring/Behaviours/MultiRoseSpritedSelectorApplier.cs
+++ Assets/com.gamemeanmachine.unity.windrose-spriteutils/Runtime/Authoring/Behaviours/MultiRoseSpritedSelectorApplier.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections.Generic;
 using AlephVault.Unity.SpriteUtils.Authoring.Types;
 using AlephVault.Unity.SpriteUtils.Types;
-using GameMeanMachine.Unity.WindRose.Types;
-using UnityEngine;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Visuals;
+using GameMeanMachine.Unity.WindRose.Authoring.ScriptableObjects.VisualResources;
+using GameMeanMachine.Unity.WindRose.SpriteUtils.Types;
 
 
 namespace GameMeanMachine.Unity.WindRose.SpriteUtils
@@ -12,30 +13,76 @@ namespace GameMeanMachine.Unity.WindRose.SpriteUtils
     {
         namespace Behaviours
         {
-            public class MultiRoseSpritedSelectorApplier : SpriteGridSelectionApplier<Dictionary<Type, RoseTuple<Sprite>>>
+            /// <summary>
+            ///   Multi-State Rose-Sprited selector appliers are added on top of <see cref="MultiRoseSprited"/>
+            ///   visuals so they are able to replace the multiple sprite roses they use.
+            /// </summary>
+            public class MultiRoseSpritedSelectorApplier : SpriteGridSelectionApplier<MultiSettings<SpriteRose>>
             {
-                protected override bool IsCompatible(SpriteGridSelection<Dictionary<Type, RoseTuple<Sprite>>> selection)
+                private MultiRoseSprited multiRoseSprited;
+
+                private void Awake()
                 {
-                    // Test compatibility (having those state behaviours).
-                    return base.IsCompatible(selection);
+                    multiRoseSprited = GetComponent<MultiRoseSprited>();
                 }
 
-                protected override void BeforeUse(SpriteGridSelection<Dictionary<Type, RoseTuple<Sprite>>> selection)
+                /// <summary>
+                ///   Tests whether the required states are present or not.
+                /// </summary>
+                /// <param name="selection">The selection to test</param>
+                /// <returns>Whether all the states in the selection exist in the multi-state</returns>
+                protected override bool IsCompatible(SpriteGridSelection<MultiSettings<SpriteRose>> selection)
                 {
-                    // Ensure compatibility, or fail (having those state behaviours).
-                    base.BeforeUse(selection);
+                    foreach (string key in selection.GetSelection().Item2.Keys)
+                    {
+                        if (!multiRoseSprited.HasState(key)) return false;
+                    }
+
+                    return true;
                 }
 
-                protected override void AfterUse(SpriteGridSelection<Dictionary<Type, RoseTuple<Sprite>>> selection)
+                /// <summary>
+                ///   Checks that the required states are present, or fails.
+                /// </summary>
+                /// <param name="selection">The selection to apply</param>
+                /// <exception cref="IncompatibleSelectionException">
+                ///   At least a state in the selection does not exist in the multi-state
+                /// </exception>
+                protected override void BeforeUse(SpriteGridSelection<MultiSettings<SpriteRose>> selection)
                 {
-                    // Apply the value.
-                    throw new NotImplementedException();
+                    foreach (string key in selection.GetSelection().Item2.Keys)
+                    {
+                        if (!multiRoseSprited.HasState(key)) throw new IncompatibleSelectionException(
+                            $"The given selection requires the state with name '{key}' to " +
+                            $"be present in the current visual object"
+                        );
+                    }
                 }
 
-                protected override void AfterRelease(SpriteGridSelection<Dictionary<Type, RoseTuple<Sprite>>> selection)
+                /// <summary>
+                ///   Replaces all the states with the idle one and the added ones.
+                /// </summary>
+                /// <param name="selection">The selection to apply</param>
+                protected override void AfterUse(SpriteGridSelection<MultiSettings<SpriteRose>> selection)
                 {
-                    // Clear the value.
-                    throw new NotImplementedException();
+                    multiRoseSprited.ReplaceState(MultiRoseSprited.IDLE, selection.GetSelection().Item1);
+                    foreach (KeyValuePair<string, Tuple<SpriteRose, string>> item in selection.GetSelection().Item2)
+                    {
+                        multiRoseSprited.ReplaceState(item.Key, item.Value.Item1);
+                    }
+                }
+
+                /// <summary>
+                ///   Clears all the states as specified in the selection.
+                /// </summary>
+                /// <param name="selection">The selection being removed</param>
+                protected override void AfterRelease(SpriteGridSelection<MultiSettings<SpriteRose>> selection)
+                {
+                    multiRoseSprited.ReplaceState(MultiRoseSprited.IDLE, null);
+                    foreach (KeyValuePair<string, Tuple<SpriteRose, string>> item in selection.GetSelection().Item2)
+                    {
+                        multiRoseSprited.ReplaceState(item.Key, item.Value.Item1);
+                    }
                 }
             }
         }
--- Assets/com.gamemeanmachine.unity.windrose-spriteutils/Runtime/Authoring/Behaviours/MultiSpritedSelectorApplier.cs
+++ Assets/com.gamemeanmachine.unity.windrose-spriteutils/Runtime/Authoring/Behaviours/MultiSpritedSelectorApplier.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections.Generic;
 using AlephVault.Unity.SpriteUtils.Authoring.Types;
 using AlephVault.Unity.SpriteUtils.Types;
-using GameMeanMachine.Unity.WindRose.Types;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.Entities.Visuals;
+using GameMeanMachine.Unity.WindRose.SpriteUtils.Types;
 using UnityEngine;
 
 
@@ -12,30 +13,77 @@ namespace GameMeanMachine.Unity.WindRose.SpriteUtils
     {
         namespace Behaviours
         {
-            public class MultiSpritedSelectorApplier : SpriteGridSelectionApplier<Dictionary<Type, Sprite>>
+            /// <summary>
+            ///   Multi-State Sprited selector appliers are added on top of <see cref="MultiSprited"/>
+            ///   visuals so they are able to replace the multiple sprites they use.
+            /// </summary>
+            [RequireComponent(typeof(MultiSprited))]
+            public class MultiSpritedSelectorApplier : SpriteGridSelectionApplier<MultiSettings<Sprite>>
             {
-                protected override bool IsCompatible(SpriteGridSelection<Dictionary<Type, Sprite>> selection)
+                private MultiSprited multiSprited;
+
+                private void Awake()
                 {
-                    // Test compatibility (having those state behaviours).
-                    return base.IsCompatible(selection);
+                    multiSprited = GetComponent<MultiSprited>();
                 }
 
-                protected override void BeforeUse(SpriteGridSelection<Dictionary<Type, Sprite>> selection)
+                /// <summary>
+                ///   Tests whether the required states are present or not.
+                /// </summary>
+                /// <param name="selection">The selection to test</param>
+                /// <returns>Whether all the states in the selection exist in the multi-state</returns>
+                protected override bool IsCompatible(SpriteGridSelection<MultiSettings<Sprite>> selection)
                 {
-                    // Ensure compatibility, or fail (having those state behaviours).
-                    base.BeforeUse(selection);
+                    foreach (string key in selection.GetSelection().Item2.Keys)
+                    {
+                        if (!multiSprited.HasState(key)) return false;
+                    }
+
+                    return true;
                 }
 
-                protected override void AfterUse(SpriteGridSelection<Dictionary<Type, Sprite>> selection)
+                /// <summary>
+                ///   Checks that the required states are present, or fails.
+                /// </summary>
+                /// <param name="selection">The selection to apply</param>
+                /// <exception cref="IncompatibleSelectionException">
+                ///   At least a state in the selection does not exist in the multi-state
+                /// </exception>
+                protected override void BeforeUse(SpriteGridSelection<MultiSettings<Sprite>> selection)
                 {
-                    // Apply the value.
-                    throw new NotImplementedException();
+                    foreach (string key in selection.GetSelection().Item2.Keys)
+                    {
+                        if (!multiSprited.HasState(key)) throw new IncompatibleSelectionException(
+                            $"The given selection requires the state with name '{key}' to " +
+                            $"be present in the current visual object"
+                        );
+                    }
                 }
 
-                protected override void AfterRelease(SpriteGridSelection<Dictionary<Type, Sprite>> selection)
+                /// <summary>
+                ///   Replaces all the states with the idle one and the added ones.
+                /// </summary>
+                /// <param name="selection">The selection to apply</param>
+                protected override void AfterUse(SpriteGridSelection<MultiSettings<Sprite>> selection)
                 {
-                    // Clear the value.
-                    throw new NotImplementedException();
+                    multiSprited.ReplaceState(MultiSprited.IDLE, selection.GetSelection().Item1);
+                    foreach (KeyValuePair<string, Tuple<Sprite, string>> item in selection.GetSelection().Item2)
+                    {
+                        multiSprited.ReplaceState(item.Key, item.Value.Item1);
+                    }
+                }
+
+                /// <summary>
+                ///   Clears all the states as specified in the selection.
+                /// </summary>
+                /// <param name="selection">The selection being removed</param>
+                protected override void AfterRelease(SpriteGridSelection<MultiSettings<Sprite>> selection)
+                {
+                    multiSprited.ReplaceState(MultiSprited.IDLE, null);
+                    foreach (KeyValuePair<string, Tuple<Sprite, string>> item in selection.GetSelection().Item2)
+                    {
+                        multiSprited.ReplaceState(item.Key, item.Value.Item1);
+                    }
                 }
             }
         }

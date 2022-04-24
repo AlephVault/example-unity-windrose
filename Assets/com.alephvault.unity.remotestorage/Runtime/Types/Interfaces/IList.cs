28008ac47da68d7f312cd2f722835cb274e78bda
Luis Masuelli <luis@datagran.io>
2022-04-23 23:49:43 -0500

    Removing folders not needed anymore, and adding simpler files replacing them.

---
+++ Assets/com.alephvault.unity.remotestorage/Runtime/Types/Interfaces/IList.cs
@@ -0,0 +1,12 @@
+namespace AlephVault.Unity.RemoteStorage
+{
+    namespace Types
+    {
+        namespace Interfaces
+        {
+            public interface IList<AuthType, ElementType, IDType, CursorType>
+            {
+            }
+        }
+    }
+}
---
+++ Assets/com.alephvault.unity.remotestorage/Runtime/Types/Interfaces/ISimple.cs
@@ -0,0 +1,13 @@
+namespace AlephVault.Unity.RemoteStorage
+{
+    namespace Types
+    {
+        namespace Interfaces
+        {
+            public interface ISimple<AuthType, ElementType, IDType>
+            {
+                public IResult
+            }
+        }
+    }
+}

26066ed8201e14e43637370037fd40190ed82511
Luis Masuelli <luis@datagran.io>
2022-05-28 14:53:53 -0500

    For the RefMapUtils, adding the ability to also render a texture from a Composite object.

--- Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Core/RefMapUtils.cs
+++ Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Core/RefMapUtils.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using AlephVault.Unity.TextureUtils.Types;
 using AlephVault.Unity.TextureUtils.Utils;
@@ -5,7 +6,7 @@ using GameMeanMachine.Unity.RefMapChars.Types;
 using UnityEngine;
 
 
-namespace CoreFoundation
+namespace Core
 {
     /// <summary>
     ///   The core function is implemented here, which takes the
@@ -14,7 +15,7 @@ namespace CoreFoundation
     public static class RefMapUtils
     {
         /// <summary>
-        ///   Provides a target texture and the relevant parts of a character
+        ///   Takes a target texture and the relevant parts of a character
         ///   that may be built using RefMap textures to build the final
         ///   character texture asset. All the parts are optional, even the
         ///   body of the character (e.g. for partial invisibility).
@@ -44,6 +45,8 @@ namespace CoreFoundation
         /// <param name="necklaceOverLongShirt">
         ///   Whether the necklace should be rendered on top of the long shirt
         /// </param>
+        /// <exception cref="ArgumentNullException">A null target is provided</exception>
+        /// <exception cref="ArgumentException">A target not being 128x192 is provided</exception>
         public static void Paste(
             Texture target, RefMapSource skilledHandItem, RefMapSource dumbHandItem, RefMapSource hat,
             RefMapSource cloak, RefMapSource hair, RefMapSource hairTail, RefMapSource arms, RefMapSource waist,
@@ -53,6 +56,11 @@ namespace CoreFoundation
             bool necklaceOverLongShirt = false
         )
         {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            if (target.width != 128 || target.height != 192)
+            {
+                throw new ArgumentException("The target texture must be 128x192");
+            }
             List<Texture2DSource> elements = new List<Texture2DSource>();
             AddIfPresent(elements, skilledHandItem?.ToTexture2DSource(maskU));
             AddIfPresent(elements, dumbHandItem?.ToTexture2DSource(maskLRU));
@@ -76,6 +84,31 @@ namespace CoreFoundation
             AddIfPresent(elements, dumbHandItem?.ToTexture2DSource(maskD));
             AddIfPresent(elements, skilledHandItem?.ToTexture2DSource(maskD));
             Textures.Paste2D(target, true, elements.ToArray());
+        }
+
+        /// <summary>
+        ///   Takes a target texture and a composite object to paste all the
+        ///   parts (in the appropriate order and using the appropriate masks)
+        ///   into it, to form a new texture (to be cached and used).
+        /// </summary>
+        /// <param name="target">The target texture to render everything into</param>
+        /// <param name="composite">The composite object to render</param>
+        /// <param name="maskD">A mask that only shows down-oriented frames</param>
+        /// <param name="maskLRU">A mask that does not show down-oriented frames</param>
+        /// <param name="maskLR">A mask that only shows side-oriented frames</param>
+        /// <param name="maskU">A mask that only shows up-oriented frames</param>
+        public static void Paste(
+            Texture target, RefMapComposite composite, Texture2D maskD, Texture2D maskLRU,
+            Texture2D maskLR, Texture2D maskU
+        ) {
+            Paste(
+                target, composite?.SkilledHandItem, composite?.DumbHandItem, composite?.Hat,
+                composite?.Cloak, composite?.Hair, composite?.HairTail, composite?.Arms,
+                composite?.Waist, composite?.Shoulder, composite?.Necklace, composite?.LongShirt,
+                composite?.Chest, composite?.Shirt, composite?.Boots, composite?.Pants, composite?.Body,
+                maskD, maskLRU, maskLR, maskU, composite?.BootsOverPants ?? false,
+                composite?.NecklaceOverLongShirt ?? false
+            );
         }
 
         private static void AddIfPresent(List<Texture2DSource> into, Texture2DSource element)

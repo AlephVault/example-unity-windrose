651f8eb29c74fda75e54b6a2f2dfd7a36e2a34a8
Luis Masuelli <luis@datagran.io>
2022-05-29 12:05:47 -0500

    Updating dependencies and also the RefMapUtils to use certain signatures instead of the former ones (notably, one for the Simple Composite object).

--- Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Core/RefMapUtils.cs
+++ Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Core/RefMapUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AlephVault.Unity.TextureUtils.Types;
 using AlephVault.Unity.TextureUtils.Utils;
 using GameMeanMachine.Unity.RefMapChars.Types;
@@ -19,78 +20,19 @@ namespace GameMeanMachine.Unity.RefMapChars
         /// </summary>
         public static class RefMapUtils
         {
-            /// <summary>
-            ///   Takes a target texture and the relevant parts of a character
-            ///   that may be built using RefMap textures to build the final
-            ///   character texture asset. All the parts are optional, even the
-            ///   body of the character (e.g. for partial invisibility).
-            /// </summary>
-            /// <param name="target">The target texture to render everything into</param>
-            /// <param name="skilledHandItem">The weapon or tool in the right / skilled hand</param>
-            /// <param name="dumbHandItem">The shield or tool in the left / dumb hand</param>
-            /// <param name="hat">The hat</param>
-            /// <param name="cloak">The cloak</param>
-            /// <param name="hair">The hair</param>
-            /// <param name="hairTail">For long hairs, the "tail" of that hair</param>
-            /// <param name="arms">The arms (clothes' extensions)</param>
-            /// <param name="waist">The waist</param>
-            /// <param name="shoulder">The shoulders (clothes' extensions)</param>
-            /// <param name="necklace">The necklace</param>
-            /// <param name="longShirt">The long shirt / tunic</param>
-            /// <param name="chest">The chest (a light armor)</param>
-            /// <param name="shirt">The shirt</param>
-            /// <param name="boots">The boots / shoes</param>
-            /// <param name="pants">The pants</param>
-            /// <param name="body">The overall body</param>
-            /// <param name="maskD">A mask that only shows down-oriented frames</param>
-            /// <param name="maskLRU">A mask that does not show down-oriented frames</param>
-            /// <param name="maskLR">A mask that only shows side-oriented frames</param>
-            /// <param name="maskU">A mask that only shows up-oriented frames</param>
-            /// <param name="bootsOverPants">Whether the boots should be rendered on top of the pants</param>
-            /// <param name="necklaceOverLongShirt">
-            ///   Whether the necklace should be rendered on top of the long shirt
-            /// </param>
-            /// <exception cref="ArgumentNullException">A null target is provided</exception>
-            /// <exception cref="ArgumentException">A target not being 128x192 is provided</exception>
-            public static void Paste(
-                Texture target, RefMapSource skilledHandItem, RefMapSource dumbHandItem, RefMapSource hat,
-                RefMapSource cloak, RefMapSource hair, RefMapSource hairTail, RefMapSource arms, RefMapSource waist,
-                RefMapSource shoulder, RefMapSource necklace, RefMapSource longShirt, RefMapSource chest,
-                RefMapSource shirt, RefMapSource boots, RefMapSource pants, RefMapSource body, Texture2D maskD,
-                Texture2D maskLRU, Texture2D maskLR, Texture2D maskU, bool bootsOverPants = false,
-                bool necklaceOverLongShirt = false
-            )
+            private static void Paste(Texture target, params Texture2DSource[] sources)
             {
                 if (target == null) throw new ArgumentNullException(nameof(target));
                 if (target.width != 128 || target.height != 192)
                 {
                     throw new ArgumentException("The target texture must be 128x192");
                 }
-                List<Texture2DSource> elements = new List<Texture2DSource>();
-                AddIfPresent(elements, skilledHandItem?.ToTexture2DSource(maskU));
-                AddIfPresent(elements, dumbHandItem?.ToTexture2DSource(maskLRU));
-                AddIfPresent(elements, hairTail?.ToTexture2DSource(maskD));
-                AddIfPresent(elements, cloak?.ToTexture2DSource(maskD));
-                AddIfPresent(elements, body?.ToTexture2DSource());
-                AddIfPresent(elements, (bootsOverPants ? pants : boots)?.ToTexture2DSource());
-                AddIfPresent(elements, (bootsOverPants ? boots : pants)?.ToTexture2DSource());
-                AddIfPresent(elements, shirt?.ToTexture2DSource());
-                AddIfPresent(elements, chest?.ToTexture2DSource());
-                AddIfPresent(elements, (necklaceOverLongShirt ? longShirt : necklace)?.ToTexture2DSource());
-                AddIfPresent(elements, (necklaceOverLongShirt ? necklace : longShirt)?.ToTexture2DSource());
-                AddIfPresent(elements, shoulder?.ToTexture2DSource());
-                AddIfPresent(elements, waist?.ToTexture2DSource());
-                AddIfPresent(elements, arms?.ToTexture2DSource());
-                AddIfPresent(elements, hair?.ToTexture2DSource());
-                AddIfPresent(elements, skilledHandItem?.ToTexture2DSource(maskLR));
-                AddIfPresent(elements, cloak?.ToTexture2DSource(maskLRU));
-                AddIfPresent(elements, hairTail?.ToTexture2DSource(maskLRU));
-                AddIfPresent(elements, hat?.ToTexture2DSource());
-                AddIfPresent(elements, dumbHandItem?.ToTexture2DSource(maskD));
-                AddIfPresent(elements, skilledHandItem?.ToTexture2DSource(maskD));
-                Textures.Paste2D(target, true, elements.ToArray());
+                Textures.Paste2D(target, true, (
+                    from source in sources
+                    where source != null select source
+                ).ToArray());
             }
-
+            
             /// <summary>
             ///   Takes a target texture and a composite object to paste all the
             ///   parts (in the appropriate order and using the appropriate masks)
@@ -105,20 +47,71 @@ namespace GameMeanMachine.Unity.RefMapChars
             public static void Paste(
                 Texture target, IRefMapComposite composite, Texture2D maskD, Texture2D maskLRU,
                 Texture2D maskLR, Texture2D maskU
-            ) {
+            )
+            {
                 Paste(
-                    target, composite?.SkilledHandItem, composite?.DumbHandItem, composite?.Hat,
-                    composite?.Cloak, composite?.Hair, composite?.HairTail, composite?.Arms,
-                    composite?.Waist, composite?.Shoulder, composite?.Necklace, composite?.LongShirt,
-                    composite?.Chest, composite?.Shirt, composite?.Boots, composite?.Pants, composite?.Body,
-                    maskD, maskLRU, maskLR, maskU, composite?.BootsOverPants ?? false,
-                    composite?.NecklaceOverLongShirt ?? false
+                    target,
+                    composite?.SkilledHandItem?.ToTexture2DSource(maskU),
+                    composite?.DumbHandItem?.ToTexture2DSource(maskLRU),
+                    composite?.HairTail?.ToTexture2DSource(maskD),
+                    composite?.Cloak?.ToTexture2DSource(maskD),
+                    composite?.Body?.ToTexture2DSource(),
+                    (composite?.BootsOverPants ?? false ?
+                        composite.Pants : composite?.Boots)?.ToTexture2DSource(),
+                    (composite?.BootsOverPants ?? false ?
+                        composite.Boots : composite?.Pants)?.ToTexture2DSource(),
+                    composite?.Shirt?.ToTexture2DSource(),
+                    composite?.Chest?.ToTexture2DSource(),
+                    (composite?.NecklaceOverLongShirt ?? false ?
+                        composite.LongShirt : composite?.Necklace)?.ToTexture2DSource(),
+                    (composite?.NecklaceOverLongShirt ?? false ?
+                        composite.Necklace : composite?.LongShirt)?.ToTexture2DSource(),
+                    composite?.Shoulder?.ToTexture2DSource(),
+                    composite?.Waist?.ToTexture2DSource(),
+                    composite?.Arms?.ToTexture2DSource(),
+                    composite?.Hair?.ToTexture2DSource(),
+                    composite?.SkilledHandItem?.ToTexture2DSource(maskLR),
+                    composite?.Cloak?.ToTexture2DSource(maskLRU),
+                    composite?.HairTail?.ToTexture2DSource(maskLRU),
+                    composite?.Hat?.ToTexture2DSource(),
+                    composite?.DumbHandItem?.ToTexture2DSource(maskD),
+                    composite?.SkilledHandItem?.ToTexture2DSource(maskD)
                 );
             }
 
-            private static void AddIfPresent(List<Texture2DSource> into, Texture2DSource element)
+            /// <summary>
+            ///   Takes a target texture and a simple composite object to paste all the
+            ///   parts (in the appropriate order and using the appropriate masks) into
+            ///   it, to form a new texture (to be cached and used). In this mode, the
+            ///   clothes make use of less iterations since they come in a single sprite
+            ///   instead of broken in parts.
+            /// </summary>
+            /// <param name="target">The target texture to render everything into</param>
+            /// <param name="composite">The simple composite object to render</param>
+            /// <param name="maskD">A mask that only shows down-oriented frames</param>
+            /// <param name="maskLRU">A mask that does not show down-oriented frames</param>
+            /// <param name="maskLR">A mask that only shows side-oriented frames</param>
+            /// <param name="maskU">A mask that only shows up-oriented frames</param>
+            public static void Paste(
+                Texture target, IRefMapSimpleComposite composite, Texture2D maskD, Texture2D maskLRU,
+                Texture2D maskLR, Texture2D maskU
+            )
             {
-                if (element != null) into.Add(element);
+                Paste(
+                    target,
+                    composite?.SkilledHandItem?.ToTexture2DSource(maskU),
+                    composite?.DumbHandItem?.ToTexture2DSource(maskLRU),
+                    composite?.HairTail?.ToTexture2DSource(maskD),
+                    composite?.Body?.ToTexture2DSource(),
+                    composite?.Cloth?.ToTexture2DSource(),
+                    composite?.Necklace?.ToTexture2DSource(),
+                    composite?.Hair?.ToTexture2DSource(),
+                    composite?.SkilledHandItem?.ToTexture2DSource(maskLR),
+                    composite?.HairTail?.ToTexture2DSource(maskLRU),
+                    composite?.Hat?.ToTexture2DSource(),
+                    composite?.DumbHandItem?.ToTexture2DSource(maskD),
+                    composite?.SkilledHandItem?.ToTexture2DSource(maskD)
+                );
             }
         }
     }

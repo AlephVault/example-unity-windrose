e8386d149e276698e46d8878c1e815f2713f69d8
Luis Masuelli <luis@datagran.io>
2022-05-31 22:43:17 -0500

    Adding some traits.

---
+++ Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Types/Traits/DumbHandToolTrait.cs
@@ -0,0 +1,16 @@
+namespace GameMeanMachine.Unity.RefMapChars
+{
+    namespace Types
+    {
+        namespace Traits
+        {
+            /// <summary>
+            ///   The dumb hand tool trait (1 texture).
+            /// </summary>
+            public class DumbHandToolTrait : SimpleTrait
+            {
+                public DumbHandToolTrait(string hash, RefMapSource front) : base(hash, front) {}
+            }
+        }
+    }
+}

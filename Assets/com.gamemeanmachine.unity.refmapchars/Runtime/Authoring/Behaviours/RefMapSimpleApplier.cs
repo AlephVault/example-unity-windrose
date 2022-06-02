0ce127ed24b1b708bc415804cc6393c4b2ee692a
Luis Masuelli <luis@datagran.io>
2022-06-01 21:57:52 -0500

    Adding the RefMapSimpleApplier.

---
+++ Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Authoring/Behaviours/RefMapSimpleApplier.cs
@@ -0,0 +1,57 @@
+using AlephVault.Unity.SpriteUtils.Types;
+using GameMeanMachine.Unity.RefMapChars.Types;
+using GameMeanMachine.Unity.RefMapChars.Types.Traits.Simple;
+
+
+namespace GameMeanMachine.Unity.RefMapChars
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            /// <summary>
+            ///   A simple RefMAp applier. Aside from the base traits, it
+            ///   adds a single cloth. How is the grid used, it is not
+            ///   defined in this class but in children classes.
+            /// </summary>
+            public abstract class RefMapSimpleApplier : RefMapBaseApplier, IRefMapSimpleComposite
+            {
+                /// <summary>
+                ///   The cloth image.
+                /// </summary>
+                public RefMapSource Cloth => clothTrait?.Front;
+
+                /// <summary>
+                ///   The cloth trait.
+                /// </summary>
+                protected ClothTrait clothTrait;
+                
+                /// <summary>
+                ///   The hash involves the 7 parts.
+                /// </summary>
+                public override string Hash()
+                {
+                    return $"{bodyTrait?.Hash ?? ""}:{hairTrait?.Hash ?? ""}:{necklaceTrait?.Hash ?? ""}:" +
+                           $"{hatTrait?.Hash ?? ""}:{skilledHandItemTrait?.Hash ?? ""}:" +
+                           $"{dumbHandItemTrait?.Hash ?? ""}:{clothTrait?.Hash ?? ""}";
+                }
+
+                /// <summary>
+                ///   Gets the grid, and uses it.
+                /// </summary>
+                protected override void RefreshTexture()
+                {
+                    UseGrid(cache.Get(this));
+                }
+
+                /// <summary>
+                ///   Using the grid depends on the selection type
+                ///   to make use of, and that depends on the type
+                ///   of visual to tie this behaviour to.
+                /// </summary>
+                /// <param name="grid">The grid to use</param>
+                protected abstract void UseGrid(SpriteGrid grid);
+            }
+        }
+    }
+}

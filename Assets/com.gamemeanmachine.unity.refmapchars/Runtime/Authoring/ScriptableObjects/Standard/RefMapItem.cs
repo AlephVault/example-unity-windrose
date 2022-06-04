732f40f7f9c543b82ced6606b02db39d6ed841a6
Luis Masuelli <luis@datagran.io>
2022-06-04 09:17:09 -0500

    Adding the RefMapItem, which can have 10 variations (colors).

---
+++ Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Authoring/ScriptableObjects/Standard/RefMapItem.cs
@@ -0,0 +1,69 @@
+using System;
+using AlephVault.Unity.Support.Generic.Authoring.Types;
+using GameMeanMachine.Unity.RefMapChars.Types;
+using UnityEngine;
+
+
+namespace GameMeanMachine.Unity.RefMapChars
+{
+    namespace Authoring
+    {
+        namespace ScriptableObjects
+        {
+            namespace Standard
+            {
+                /// <summary>
+                ///   A list of the available sources, given an index.
+                ///   These are intended to categorize a single object.
+                ///   Only 10 variations are allowed. An item belongs
+                ///   to a certain sex trait, and a certain asset type.
+                ///   An item may, however, be actually a part of a
+                ///   bigger trait, whose parts are scattered (e.g.
+                ///   a front hair and a back hair would be implemented
+                ///   in two different items, but working as a complement
+                ///   of each other respectively).
+                /// </summary>
+                public class RefMapItem : ScriptableObject
+                {
+                    /// <summary>
+                    ///   Each item or body trait (other than the body itself)
+                    ///   has 10 possible colors.
+                    /// </summary>
+                    public enum Color : byte
+                    {
+                        Black,
+                        Blue,
+                        DarkBrown,
+                        Green,
+                        LightBrown,
+                        Pink,
+                        Purple,
+                        Red,
+                        White,
+                        Yellow
+                    }
+
+                    /// <summary>
+                    ///   The dictionary to use (maps a byte code to a ref
+                    ///   map source).
+                    /// </summary>
+                    [Serializable]
+                    public class RefMapVariationsDictionary : Dictionary<Color, RefMapSource> {}
+                
+                    /// <summary>
+                    ///   A dictionary of the variations to use for this
+                    ///   graphical asset.
+                    /// </summary>
+                    [SerializeField]
+                    private RefMapVariationsDictionary variations = new RefMapVariationsDictionary();
+
+                    /// <summary>
+                    ///   Gets a <see cref="RefMapSource"/> at a given index.
+                    /// </summary>
+                    /// <param name="index">The index to retrieve the source for</param>
+                    public RefMapSource this[Color index] => variations[index];
+                }
+            }
+        }
+    }    
+}

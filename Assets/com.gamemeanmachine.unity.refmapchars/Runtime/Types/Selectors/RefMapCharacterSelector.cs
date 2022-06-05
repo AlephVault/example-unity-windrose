d6216e7f05095872932c0a03624705373fec1f95
Luis Masuelli <luis@datagran.io>
2022-06-05 11:43:20 -0500

    Fixing the indices.

--- Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Types/Selectors/RefMapCharacterSelector.cs
+++ Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Types/Selectors/RefMapCharacterSelector.cs
@@ -25,31 +25,31 @@ namespace GameMeanMachine.Unity.RefMapChars
                     new MultiSettings<RoseTuple<ReadOnlyCollection<Vector2Int>>>
                     {
                         { MapObject.IDLE_STATE, new RoseTuple<ReadOnlyCollection<Vector2Int>>(
-                            Array.AsReadOnly(new []{ new Vector2Int(0, 0) }),
+                            Array.AsReadOnly(new []{ new Vector2Int(0, 3) }),
                             Array.AsReadOnly(new []{ new Vector2Int(0, 1) }),
                             Array.AsReadOnly(new []{ new Vector2Int(0, 2) }),
-                            Array.AsReadOnly(new []{ new Vector2Int(0, 3) })
+                            Array.AsReadOnly(new []{ new Vector2Int(0, 0) })
                           )},
                         { MapObject.MOVING_STATE, new RoseTuple<ReadOnlyCollection<Vector2Int>>(
                             Array.AsReadOnly(new []
                             {
-                                new Vector2Int(0, 0), new Vector2Int(0, 1),
-                                new Vector2Int(0, 2), new Vector2Int(0, 3)                                
+                                new Vector2Int(0, 3), new Vector2Int(1, 3),
+                                new Vector2Int(2, 3), new Vector2Int(3, 3)                                
                             }),
                             Array.AsReadOnly(new []
                             {
-                                new Vector2Int(1, 0), new Vector2Int(1, 1),
-                                new Vector2Int(1, 2), new Vector2Int(1, 3)
+                                new Vector2Int(0, 1), new Vector2Int(1, 1),
+                                new Vector2Int(2, 1), new Vector2Int(3, 1)
                             }),
                             Array.AsReadOnly(new []
                             {
-                                new Vector2Int(2, 0), new Vector2Int(2, 1),
-                                new Vector2Int(2, 2), new Vector2Int(2, 3)
+                                new Vector2Int(0, 2), new Vector2Int(1, 2),
+                                new Vector2Int(2, 2), new Vector2Int(3, 2)
                             }),
                             Array.AsReadOnly(new []
                             {
-                                new Vector2Int(3, 0), new Vector2Int(3, 1),
-                                new Vector2Int(3, 2), new Vector2Int(3, 3)
+                                new Vector2Int(0, 0), new Vector2Int(1, 0),
+                                new Vector2Int(2, 0), new Vector2Int(3, 0)
                             })
                           )}
                     },
--- Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Types/Selectors/RefMapStatueSelector.cs
+++ Assets/com.gamemeanmachine.unity.refmapchars/Runtime/Types/Selectors/RefMapStatueSelector.cs
@@ -16,8 +16,8 @@ namespace GameMeanMachine.Unity.RefMapChars
             public class RefMapStatueSelection : RoseSpritedSelection
             {
                 public RefMapStatueSelection(SpriteGrid sourceGrid) : base(sourceGrid, new RoseTuple<Vector2Int>(
-                    new Vector2Int(0, 0), new Vector2Int(0, 1), 
-                    new Vector2Int(0, 2), new Vector2Int(0, 3))
+                    new Vector2Int(0, 3), new Vector2Int(0, 1), 
+                    new Vector2Int(0, 2), new Vector2Int(0, 0))
                 )
                 {
                 }

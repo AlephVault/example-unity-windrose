2ef4ccc4480179e5c5fb461907996fd8d4fbb9f9
Luis Masuelli <luis@datagran.io>
2022-06-12 09:35:36 -0500

    Adding the ERC20 function and event models.

---
+++ Assets/com.alephvault.unity.evmgames/Samples/Scripts/EthModels/Erc20TransferEvent.cs
@@ -0,0 +1,25 @@
+using AlephVault.Unity.EVMGames.Nethereum.ABI.FunctionEncoding.Attributes;
+using Org.BouncyCastle.Math;
+
+
+namespace AlephVault.Unity.EVMGames
+{
+    namespace Samples
+    {
+        namespace EthModels
+        {
+            [Event("Transfer")]
+            public class Erc20TransferEvent : IEventDTO
+            {
+                [Parameter("address", "_from", 1)]
+                public string From { get; set; }
+        
+                [Parameter("address", "_to", 2)]        
+                public string To { get; set; }
+        
+                [Parameter("uint256", "_value", 3)]        
+                public BigInteger Value { get; set; }
+            }
+        }
+    }
+}

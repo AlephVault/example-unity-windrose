c6232c9302f9ecc04a00abac1a2d87f0c9b5a014
Luis Masuelli <luis@datagran.io>
2022-06-14 08:32:18 -0500

    Adding the "tabs" partial class.

--- Assets/com.alephvault.unity.evmgames/Samples/Scripts/SampleContractInteractor.cs
+++ Assets/com.alephvault.unity.evmgames/Samples/Scripts/SampleContractInteractor.cs
@@ -28,88 +28,16 @@ namespace AlephVault.Unity.EVMGames
             [SerializeField]
             private string endpoint;
             
-            // Panes and tab buttons go here.
-            
-            private Button pkWalletButton;
-            private Button connectedWalletButton;
-            private Button eventsButton;
-            private Transform pkWalletPanel;
-            private Transform connectedWalletPanel;
-            private Transform eventsPanel;
-            
-            // Core variables go here.
-            
-            // Now, per-pane variables go here.
-            
-            // Events pane.
-            
-            private void TogglePanel(Transform tf, bool show)
-            {
-                tf.localScale = show ? Vector3.one : Vector3.zero;
-            }
-            
-            //////////////////////////////////
-            // Methods to invoke
-            //////////////////////////////////
-            
-            
-            //////////////////////////////////
-            // Event initializers
-            //////////////////////////////////
-            
             private void Awake()
             {
-                pkWalletButton = transform.Find("pkWalletButton").GetComponent<Button>();
-                connectedWalletButton = transform.Find("connectedWalletButton").GetComponent<Button>();
-                eventsButton = transform.Find("eventsButton").GetComponent<Button>();
-                pkWalletPanel = transform.Find("pkWalletPanel");
-                connectedWalletPanel = transform.Find("connectedWalletPanel");
-                eventsPanel = transform.Find("eventsPanel");
-                TogglePanel(pkWalletPanel, false);
-                TogglePanel(connectedWalletPanel, false);
-                TogglePanel(eventsPanel, false);
-                
-                pkWalletButton.onClick.AddListener(() =>
-                {
-                    Debug.Log("pk wallet panel");
-                    TogglePanel(pkWalletPanel, true);
-                    TogglePanel(connectedWalletPanel, false);
-                    TogglePanel(eventsPanel, false);
-                });
-                connectedWalletButton.onClick.AddListener(() =>
-                {
-                    Debug.Log("connected wallet panel");
-                    TogglePanel(pkWalletPanel, false);
-                    TogglePanel(connectedWalletPanel, true);
-                    TogglePanel(eventsPanel, false);
-                });
-                eventsButton.onClick.AddListener(() =>
-                {
-                    Debug.Log("events panel");
-                    TogglePanel(pkWalletPanel, false);
-                    TogglePanel(connectedWalletPanel, false);
-                    TogglePanel(eventsPanel, true);
-                });
-                
-                
-                
-                
-                
-                // Core variables initialization goes here.
-
-                // This one is the server client. Will also be used for events.
-                
-                
-                // Now, per-pane settings go here.
-
-                // 2. Server pane.
-                // 3. Client pane.
-                
+                AwakeTabs();
+                AwakeEventsPanel();
             }
 
             private void Start()
             {
                 StartWeb3Clients();
+                StartTabs();
                 StartEventsPanel();
             }
         }

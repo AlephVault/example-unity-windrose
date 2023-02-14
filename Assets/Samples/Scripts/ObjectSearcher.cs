d2cc2035067e177ce0abc0709fb282033a40aa5b
Luis Masuelli <luis@datagran.io>
2023-02-13 22:08:08 -0500

    Adding a new test for WindRose 0.0.7

---
+++ Assets/Samples/Scripts/ObjectSearcher.cs
@@ -0,0 +1,34 @@
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.World;
+using GameMeanMachine.Unity.WindRose.Authoring.Behaviours.World.Layers.Objects;
+using UnityEngine;
+
+public class ObjectSearcher : MonoBehaviour
+{
+    private Camera camera;
+    private Map map;
+    private Grid grid;
+    private ObjectsLayer objects;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        map = GetComponent<Map>();
+        objects = map.GetComponentInChildren<ObjectsLayer>();
+        grid = objects.GetComponent<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 worldPosition = camera.ScreenToWorldPoint(mousePosition);
+            Vector3 cell = grid.WorldToCell(worldPosition);
+            Debug.Log("Searching objects...");
+            foreach(var obj in objects.StrategyHolder.Search((ushort)(int)cell.x, (ushort)(int)cell.y))
+            {
+                Debug.Log($"Name: {obj.name}");
+            }
+        }
+    }
+}

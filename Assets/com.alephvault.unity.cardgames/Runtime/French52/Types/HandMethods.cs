e7c73f9116557c6dbde2ecb0c374479c3e5774f5
Luis Masuelli <luis@datagran.io>
2023-07-21 22:51:09 -0500

    Adding the hand-preparer method a feature: to make a new hand, and to pre-process lowball modes.

--- Assets/com.alephvault.unity.cardgames/Runtime/French52/Types/HandMethods.cs
+++ Assets/com.alephvault.unity.cardgames/Runtime/French52/Types/HandMethods.cs
@@ -9,22 +9,35 @@ namespace AlephVault.Unity.CardGames
              */
             public static class HandMethods
             {
-                /**
-                 * In-place sorts the cards by their value.
-                 */
-                public static void SortByValue(this int[] hand)
+                /// <summary>
+                ///   Sorts (as new hand) the cards by their value and, for lowball
+                ///   games, the values are re-mapped: Ace (12) changes to 0 while
+                ///   all the other values are increased by 1.
+                /// </summary>
+                /// <param name="hand">The hand. Typically, 5 cards</param>
+                /// <param name="lowball">Whether to apply lowball encoding or not</param>
+                public static int[] PrepareForEvaluation(this int[] hand, bool lowball = false)
                 {
                     int length = hand.Length;
+                    int[] newHand = (int[])hand.Clone();
+                    if (lowball)
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            newHand[i] = newHand[i].Value() == 12 ? newHand[i] - 12 : newHand[i] + 1;
+                        }
+                    }
                     for (int i = 0; i < length; i++)
                     {
                         for (int j = i + 1; j < length; j++)
                         {
-                            if (hand[i].Value() < hand[j].Value())
+                            if (newHand[i].Value() < newHand[j].Value())
                             {
-                                (hand[i], hand[j]) = (hand[j], hand[i]);
+                                (newHand[i], newHand[j]) = (newHand[j], newHand[i]);
                             }
                         }
                     }
+                    return newHand;
                 }
             }
         }

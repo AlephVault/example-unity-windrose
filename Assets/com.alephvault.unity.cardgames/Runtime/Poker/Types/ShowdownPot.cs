38aeab3f1af14d7f5695c958882b26d3c95dd6fc
Luis Masuelli <luis@datagran.io>
2023-07-22 16:25:40 -0500

    Adding the showdown pot class.

---
+++ Assets/com.alephvault.unity.cardgames/Runtime/Poker/Types/ShowdownPot.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+
+namespace AlephVault.Unity.CardGames
+{
+    namespace Poker
+    {
+        namespace Types
+        {
+            /// <summary>
+            ///   A showdown pot is either the main one (1) or a side pot.
+            ///   Knows its total amount and the involved players (agents).
+            /// </summary>
+            public class ShowdownPot
+            {
+                /// <summary>
+                ///   The amount of each pot.
+                /// </summary>
+                public readonly int EachPot;
+
+                /// <summary>
+                ///   The amount of the total pot.
+                /// </summary>
+                public readonly int TotalPot;
+
+                /// <summary>
+                ///   The agents.
+                /// </summary>
+                public readonly IReadOnlyList<IAgent> Agents;
+
+                /// <summary>
+                ///   Builds the sidepot with the amount and the players.
+                /// </summary>
+                /// <param name="eachPot">The size of each pot</param>
+                /// <param name="agents">The involved agents</param>
+                public ShowdownPot(int eachPot, IReadOnlyList<IAgent> agents)
+                {
+                    EachPot = eachPot;
+                    TotalPot = eachPot * agents.Count;
+                    Agents = agents;
+                }
+            }
+        }
+    }
+}

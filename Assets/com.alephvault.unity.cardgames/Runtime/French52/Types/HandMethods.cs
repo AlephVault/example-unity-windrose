7161b097dda544e8b3f939d480f486e19bc4792c
Luis Masuelli <luis@datagran.io>
2023-07-21 23:36:20 -0500

    Adding the methods to test suited or straight.

--- Assets/com.alephvault.unity.cardgames/Runtime/French52/Types/HandMethods.cs
+++ Assets/com.alephvault.unity.cardgames/Runtime/French52/Types/HandMethods.cs
@@ -37,7 +37,57 @@ namespace AlephVault.Unity.CardGames
                             }
                         }
                     }
+
                     return newHand;
+                }
+
+                /// <summary>
+                ///   Tells whether a hand is suited.
+                /// </summary>
+                /// <param name="hand">The hand to evaluate</param>
+                /// <returns>Whether it is on-suit or not</returns>
+                public static bool IsSuited(this int[] hand)
+                {
+                    int length = hand.Length;
+                    if (length == 0) return true;
+                    int suit = hand[0].Suit();
+                    for (int i = 1; i < length; i++)
+                    {
+                        if (suit != hand[i].Suit()) return false;
+                    }
+
+                    return true;
+                }
+
+                /// <summary>
+                ///   Tells whether a hand is straight or not.
+                /// </summary>
+                /// <param name="hand">The hand to evaluate</param>
+                /// <param name="lowball">Whether to use lowball mode</param>
+                /// <returns>Whether it is straight or not</returns>
+                public static bool IsStraight(this int[] hand, bool lowball = false)
+                {
+                    int length = hand.Length;
+                    if (length == 0) return true;
+                    
+                    // First, a natural straight: T, T-1, T-2, ...
+                    int top = hand[0].Value();
+                    bool natural = true;
+                    for (int i = 1; i < length; i++)
+                    {
+                        if (hand[i].Value() != top - i)
+                        {
+                            natural = false;
+                            break;
+                        }
+                    }
+                    if (natural) return true;
+
+                    // In standard modes, (12 3 2 1 0) stands for (A 5 4 3 2)
+                    // which is an accepted lowest straight. So we return false
+                    // unless this is standard and that combination of values.
+                    return !lowball && hand[0].Value() == 12 && hand[1].Value() == 3 && hand[2].Value() == 2 &&
+                                       hand[3].Value() == 1 && hand[4].Value() == 0;
                 }
             }
         }

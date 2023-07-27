a814a45e86548765b2ac3397a57cdfc17281221a
Luis Masuelli <luis@datagran.io>
2023-07-27 13:48:36 -0500

    Adding the agent turn prompt options.

---
+++ Assets/com.alephvault.unity.cardgames/Runtime/Types/AgentTurnPromptOption.cs
@@ -0,0 +1,36 @@
+namespace AlephVault.Unity.CardGames
+{
+    namespace Types
+    {
+        /// <summary>
+        ///   Prompt actions for the agents. They'll be sent into
+        ///   an array, and they will have an in-array unique code.
+        /// </summary>
+        public class AgentTurnPromptOption
+        {
+            /// <summary>
+            ///   The prompt code.
+            /// </summary>
+            public readonly string Code;
+
+            /// <summary>
+            ///   The first argument. Some actions do not
+            ///   require any argument at all.
+            /// </summary>
+            public readonly int Arg1;
+
+            /// <summary>
+            ///   The second argument. Some actions do not
+            ///   require a second argument at all.
+            /// </summary>
+            public readonly int Arg2;
+
+            public AgentTurnPromptOption(string code, int arg1, int arg2)
+            {
+                Code = code;
+                Arg1 = arg1;
+                Arg2 = arg2;
+            }
+        }
+    }        
+}
